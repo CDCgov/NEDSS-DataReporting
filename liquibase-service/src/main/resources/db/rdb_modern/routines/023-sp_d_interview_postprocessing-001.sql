9d7d1fbc39f70d2d4383f2858356b990412ad2de
Joshua Mills <joshua.mills@cdw.com>
2024-11-22 15:02:34 -0500

    CNDE-1921: Formatting and comments

--- liquibase-service/src/main/resources/db/rdb_modern/routines/023-sp_d_interview_postprocessing-001.sql
+++ liquibase-service/src/main/resources/db/rdb_modern/routines/023-sp_d_interview_postprocessing-001.sql
@@ -13,6 +13,7 @@ BEGIN
     DECLARE @Pivot_sql NVARCHAR(MAX) = '';
     DECLARE @Insert_sql NVARCHAR(MAX) = '';
     DECLARE @Update_sql NVARCHAR(MAX) = '';
+    -- number of columns for the dynamic sql
     DECLARE @Col_number BIGINT = (SELECT COUNT(*) FROM dbo.nrt_metadata_columns);
 
     BEGIN TRY
@@ -38,13 +39,14 @@ BEGIN
         SET @PROC_STEP_NO = @PROC_STEP_NO + 1;
         SET @PROC_STEP_NAME = ' GENERATING #NEW_COLUMNS';
 
-        SELECT
-            RDB_COLUMN_NM
+        SELECT RDB_COLUMN_NM
         INTO #NEW_COLUMNS
         FROM dbo.nrt_metadata_columns
-        WHERE NEW_FLAG = 1 AND RDB_COLUMN_NM NOT IN (SELECT COLUMN_NAME
-    FROM INFORMATION_SCHEMA.COLUMNS
-    WHERE TABLE_NAME = 'D_INTERVIEW' AND TABLE_SCHEMA = 'dbo');
+        WHERE NEW_FLAG = 1
+          AND RDB_COLUMN_NM NOT IN (SELECT COLUMN_NAME
+                                    FROM INFORMATION_SCHEMA.COLUMNS
+                                    WHERE TABLE_NAME = 'D_INTERVIEW'
+                                      AND TABLE_SCHEMA = 'dbo');
 
         SELECT @ROWCOUNT_NO = @@ROWCOUNT;
 
@@ -60,17 +62,20 @@ BEGIN
         SET @PROC_STEP_NO = @PROC_STEP_NO + 1;
         SET @PROC_STEP_NAME = 'ADDING COLUMNS TO D_INTERVIEW';
 
-        SELECT @ColumnAdd_sql = STRING_AGG('ALTER TABLE dbo.D_INTERVIEW ADD ' + QUOTENAME(RDB_COLUMN_NM) + ' VARCHAR(50);', CHAR(13) + CHAR(10))
+        SELECT @ColumnAdd_sql =
+               STRING_AGG('ALTER TABLE dbo.D_INTERVIEW ADD ' + QUOTENAME(RDB_COLUMN_NM) + ' VARCHAR(50);',
+                          CHAR(13) + CHAR(10))
         FROM #NEW_COLUMNS;
 
         IF @ColumnAdd_sql IS NOT NULL
-        BEGIN
-            EXEC sp_executesql @ColumnAdd_sql;
-        END
+            BEGIN
+                EXEC sp_executesql @ColumnAdd_sql;
+            END
 
         UPDATE dbo.nrt_metadata_columns
         SET NEW_FLAG = 0
-        WHERE NEW_FLAG = 1 AND TABLE_NAME = 'D_INTERVIEW';
+        WHERE NEW_FLAG = 1
+          AND TABLE_NAME = 'D_INTERVIEW';
 
         SELECT @ROWCOUNT_NO = @@ROWCOUNT;
 
@@ -86,25 +91,24 @@ BEGIN
         SET @PROC_STEP_NO = @PROC_STEP_NO + 1;
         SET @PROC_STEP_NAME = ' GENERATING #INTERVIEW_INIT';
 
-        SELECT
-            INTERVIEW_UID,
-            interview_status_cd AS IX_STATUS_CD,
-            interview_date AS IX_DATE,
-            interviewee_role_cd AS IX_INTERVIEWEE_ROLE_CD,
-            interview_type_cd AS IX_TYPE_CD,
-            interview_loc_cd AS IX_LOCATION_CD,
-            local_id,
-            record_status_cd,
-            record_status_time,
-            ADD_TIME,
-            add_user_id,
-            last_chg_time,
-            last_chg_user_id,
-            version_ctrl_nbr,
-            IX_STATUS,
-            IX_TYPE,
-            IX_LOCATION,
-            IX_INTERVIEWEE_ROLE
+        SELECT INTERVIEW_UID,
+               interview_status_cd AS IX_STATUS_CD,
+               interview_date      AS IX_DATE,
+               interviewee_role_cd AS IX_INTERVIEWEE_ROLE_CD,
+               interview_type_cd   AS IX_TYPE_CD,
+               interview_loc_cd    AS IX_LOCATION_CD,
+               local_id,
+               record_status_cd,
+               record_status_time,
+               ADD_TIME,
+               add_user_id,
+               last_chg_time,
+               last_chg_user_id,
+               version_ctrl_nbr,
+               IX_STATUS,
+               IX_TYPE,
+               IX_LOCATION,
+               IX_INTERVIEWEE_ROLE
         INTO #INTERVIEW_INIT
         FROM dbo.nrt_interview
         WHERE interview_uid in (SELECT value FROM STRING_SPLIT(@interview_uids, ','));
@@ -128,10 +132,9 @@ BEGIN
         SET @PROC_STEP_NO = @PROC_STEP_NO + 1;
         SET @PROC_STEP_NAME = ' GENERATING #INTERVIEW_ANSWERS';
 
-        SELECT
-            interview_uid,
-            rdb_column_nm,
-            answer_val
+        SELECT interview_uid,
+               rdb_column_nm,
+               answer_val
         INTO #INTERVIEW_ANSWERS
         FROM dbo.nrt_interview_answer
         WHERE interview_uid in (SELECT value FROM STRING_SPLIT(@interview_uids, ','));
@@ -164,19 +167,19 @@ BEGIN
 
         CREATE TABLE #L_INTERVIEW_N
         (
-            [id]  [int] IDENTITY
+            [id]              [int] IDENTITY
                 (
                 1,
                 1
-                )                           NOT NULL,
-            [INTERVIEW_UID] [numeric](20, 0) NULL,
+                )                              NOT NULL,
+            [INTERVIEW_UID]   [numeric](20, 0) NULL,
             [D_INTERVIEW_KEY] [numeric](18, 0) NULL
         ) ON [PRIMARY];
 
 
         insert into #L_INTERVIEW_N ([INTERVIEW_UID])
         SELECT DISTINCT INTERVIEW_UID
-        FROM #INTERVIEW_INIT 
+        FROM #INTERVIEW_INIT
         EXCEPT
         SELECT INTERVIEW_UID
         FROM dbo.L_INTERVIEW lt;
@@ -229,9 +232,13 @@ BEGIN
         SET @PROC_STEP_NO = @PROC_STEP_NO + 1;
         SET @PROC_STEP_NAME = 'INSERT INTO D_INTERVIEW';
 
-        SET @PivotColumns = (SELECT STRING_AGG(QUOTENAME(RDB_COLUMN_NM), ',') 
-        FROM dbo.nrt_metadata_columns);
+        SET @PivotColumns = (SELECT STRING_AGG(QUOTENAME(RDB_COLUMN_NM), ',')
+                             FROM dbo.nrt_metadata_columns);
 
+        /*
+        Query is built one part after another, adding in extra parts
+        for the dynamic columns if @Col_number > 0
+        */
         SET @Insert_sql = '
         INSERT INTO dbo.D_INTERVIEW (
         D_INTERVIEW_KEY,
@@ -252,33 +259,41 @@ BEGIN
         IX_INTERVIEWEE_ROLE,
         IX_TYPE,
         IX_LOCATION
-        ' + CASE WHEN @Col_number > 0 THEN ',' + (SELECT STRING_AGG(QUOTENAME(RDB_COLUMN_NM), ',') FROM dbo.nrt_metadata_columns) + ') '
-        ELSE ')' end + 
-        ' SELECT 
-        li.D_INTERVIEW_KEY,
-        ix.IX_STATUS_CD,
-        ix.IX_DATE,
-        ix.IX_INTERVIEWEE_ROLE_CD,
-        ix.IX_TYPE_CD,
-        ix.IX_LOCATION_CD,
-        ix.LOCAL_ID,
-        ix.RECORD_STATUS_CD,
-        ix.RECORD_STATUS_TIME,
-        ix.ADD_TIME,
-        ix.ADD_USER_ID,
-        ix.LAST_CHG_TIME,
-        ix.LAST_CHG_USER_ID,
-        ix.VERSION_CTRL_NBR,
-        ix.IX_STATUS,
-        ix.IX_INTERVIEWEE_ROLE,
-        ix.IX_TYPE,
-        ix.IX_LOCATION
-        ' + CASE WHEN @Col_number > 0 THEN ',' + (SELECT STRING_AGG('pv.' + QUOTENAME(RDB_COLUMN_NM), ',') FROM dbo.nrt_metadata_columns)
-            ELSE '' END +
-        'FROM #L_INTERVIEW_N li
-        LEFT JOIN #INTERVIEW_INIT ix
-            ON li.interview_uid = ix.interview_uid'
-        + CASE WHEN @Col_number > 0 THEN ' LEFT JOIN (
+        ' + CASE
+        WHEN @Col_number > 0 THEN ',' +
+        (SELECT STRING_AGG(QUOTENAME(RDB_COLUMN_NM), ',')
+        FROM dbo.nrt_metadata_columns) + ') '
+        ELSE ')' end +
+                          ' SELECT 
+                          li.D_INTERVIEW_KEY,
+                          ix.IX_STATUS_CD,
+                          ix.IX_DATE,
+                          ix.IX_INTERVIEWEE_ROLE_CD,
+                          ix.IX_TYPE_CD,
+                          ix.IX_LOCATION_CD,
+                          ix.LOCAL_ID,
+                          ix.RECORD_STATUS_CD,
+                          ix.RECORD_STATUS_TIME,
+                          ix.ADD_TIME,
+                          ix.ADD_USER_ID,
+                          ix.LAST_CHG_TIME,
+                          ix.LAST_CHG_USER_ID,
+                          ix.VERSION_CTRL_NBR,
+                          ix.IX_STATUS,
+                          ix.IX_INTERVIEWEE_ROLE,
+                          ix.IX_TYPE,
+                          ix.IX_LOCATION
+                          ' + CASE
+                          WHEN @Col_number > 0 THEN ',' +
+                          (SELECT STRING_AGG('pv.' + QUOTENAME(RDB_COLUMN_NM), ',')
+                          FROM dbo.nrt_metadata_columns)
+                          ELSE '' END +
+                          'FROM #L_INTERVIEW_N li
+                          LEFT JOIN #INTERVIEW_INIT ix
+                              ON li.interview_uid = ix.interview_uid'
+            + CASE
+            WHEN @Col_number > 0 THEN
+         ' LEFT JOIN (
         SELECT interview_uid, ' + @PivotColumns + '
     FROM (
         SELECT 
@@ -293,8 +308,8 @@ BEGIN
         FOR rdb_column_nm IN (' + @PivotColumns + ')
     ) AS PivotTable) pv 
     ON pv.interview_uid = ix.interview_uid'
-    ELSE '' END;
-        
+        ELSE '' END;
+
 
         exec sp_executesql @Insert_sql;
 
@@ -312,9 +327,14 @@ BEGIN
         SET @PROC_STEP_NO = @PROC_STEP_NO + 1;
         SET @PROC_STEP_NAME = 'UPDATE D_INTERVIEW';
 
-        SET @PivotColumns = (SELECT STRING_AGG(QUOTENAME(RDB_COLUMN_NM), ',') 
-        FROM dbo.nrt_metadata_columns);
+        SET @PivotColumns = (SELECT STRING_AGG(QUOTENAME(RDB_COLUMN_NM), ',')
+                             FROM dbo.nrt_metadata_columns);
 
+
+        /*
+        Query is built one part after another, adding in extra parts
+        for the dynamic columns if @Col_number > 0
+        */
         SET @Update_sql = '
         UPDATE dl 
         SET 
@@ -335,7 +355,9 @@ BEGIN
         dl.IX_INTERVIEWEE_ROLE = ix.IX_INTERVIEWEE_ROLE,
         dl.IX_TYPE = ix.IX_TYPE,
         dl.IX_LOCATION = ix.IX_LOCATION 
-        ' + CASE WHEN @Col_number > 0 THEN ',' + (SELECT STRING_AGG('dl.' + QUOTENAME(RDB_COLUMN_NM) + ' = pv.' + QUOTENAME(RDB_COLUMN_NM), ',') FROM dbo.nrt_metadata_columns)
+        ' + CASE
+                WHEN @Col_number > 0 THEN ',' + (SELECT STRING_AGG('dl.' + QUOTENAME(RDB_COLUMN_NM) + ' = pv.' + QUOTENAME(RDB_COLUMN_NM),',')
+                                                 FROM dbo.nrt_metadata_columns)
             ELSE '' END +
         ' FROM 
         #INTERVIEW_INIT ix
@@ -343,25 +365,27 @@ BEGIN
             ON ix.interview_uid = lint.interview_uid
         LEFT JOIN dbo.D_INTERVIEW dl
             ON lint.d_interview_key = dl.d_interview_key '
-        + CASE WHEN @Col_number > 0 THEN ' LEFT JOIN (
+        + CASE
+              WHEN @Col_number > 0 THEN 
+        ' LEFT JOIN (
         SELECT interview_uid, ' + @PivotColumns + '
-    FROM (
-        SELECT 
-            interview_uid, 
-            rdb_column_nm, 
-            answer_val
-        FROM 
-            #INTERVIEW_ANSWERS
-    ) AS SourceData
-    PIVOT (
-        MAX(answer_val) 
-        FOR rdb_column_nm IN (' + @PivotColumns + ')
-    ) AS PivotTable) pv 
-     ON pv.interview_uid = ix.interview_uid'
-    ELSE ' ' END
-    + ' WHERE
+        FROM (
+            SELECT 
+                interview_uid, 
+                rdb_column_nm, 
+                answer_val
+            FROM 
+                #INTERVIEW_ANSWERS
+        ) AS SourceData
+        PIVOT (
+            MAX(answer_val) 
+            FOR rdb_column_nm IN (' + @PivotColumns + ')
+        ) AS PivotTable) pv 
+        ON pv.interview_uid = ix.interview_uid'
+        ELSE ' ' END + 
+        ' WHERE
         ix.interview_uid NOT IN (SELECT interview_uid FROM #L_INTERVIEW_N);';
-        
+
         SELECT @Update_sql;
 
         exec sp_executesql @Update_sql;
@@ -376,27 +400,25 @@ BEGIN
         COMMIT TRANSACTION;
 
 
-
         BEGIN TRANSACTION;
 
         SET @PROC_STEP_NO = @PROC_STEP_NO + 1;
         SET @PROC_STEP_NAME = 'GENERATING #INTERVIEW_NOTE_INIT';
 
-        SELECT
-            ixn.interview_uid,
-            lint.d_interview_key,
-            ixn.nbs_answer_uid,
-            ixn.user_first_name,
-            ixn.user_last_name,
-            ixn.user_comment,
-            ixn.comment_date
+        SELECT ixn.interview_uid,
+               lint.d_interview_key,
+               ixn.nbs_answer_uid,
+               ixn.user_first_name,
+               ixn.user_last_name,
+               ixn.user_comment,
+               ixn.comment_date
         INTO #INTERVIEW_NOTE_INIT
         FROM dbo.nrt_interview_note ixn
-        LEFT JOIN dbo.L_INTERVIEW lint
-            ON ixn.interview_uid = lint.interview_uid
+                 LEFT JOIN dbo.L_INTERVIEW lint
+                           ON ixn.interview_uid = lint.interview_uid
         WHERE ixn.interview_uid in (SELECT value FROM STRING_SPLIT(@interview_uids, ','));
 
-        
+
         if
             @debug = 'true'
             select @Proc_Step_Name as step, *
@@ -426,20 +448,20 @@ BEGIN
 
         CREATE TABLE #L_INTERVIEW_NOTE_N
         (
-            [id]  [int] IDENTITY
+            [id]                   [int] IDENTITY
                 (
                 1,
                 1
-                )                           NOT NULL,
-            [D_INTERVIEW_KEY] [numeric](20, 0) NULL,
+                )                                   NOT NULL,
+            [D_INTERVIEW_KEY]      [numeric](20, 0) NULL,
             [D_INTERVIEW_NOTE_KEY] [numeric](18, 0) NULL,
-            [NBS_ANSWER_UID] [numeric](18, 0) NULL
+            [NBS_ANSWER_UID]       [numeric](18, 0) NULL
         ) ON [PRIMARY];
 
 
         insert into #L_INTERVIEW_NOTE_N (D_INTERVIEW_KEY, NBS_ANSWER_UID)
         SELECT DISTINCT D_INTERVIEW_KEY, NBS_ANSWER_UID
-        FROM #INTERVIEW_NOTE_INIT 
+        FROM #INTERVIEW_NOTE_INIT
         EXCEPT
         SELECT D_INTERVIEW_KEY, NBS_ANSWER_UID
         FROM dbo.L_INTERVIEW_NOTE;
@@ -496,27 +518,23 @@ BEGIN
             @PROC_STEP_NAME = 'INSERTING INTO D_INTERVIEW_NOTE';
 
 
-        INSERT INTO dbo.D_INTERVIEW_NOTE (
-            D_INTERVIEW_KEY,
-            D_INTERVIEW_NOTE_KEY,
-            NBS_ANSWER_UID,
-            USER_FIRST_NAME,
-            USER_LAST_NAME,
-            USER_COMMENT,
-            COMMENT_DATE
-        )
-        SELECT 
-            lintn.D_INTERVIEW_KEY,
-            lintn.D_INTERVIEW_NOTE_KEY,
-            ixn.NBS_ANSWER_UID,
-            ixn.USER_FIRST_NAME,
-            ixn.USER_LAST_NAME,
-            ixn.USER_COMMENT,
-            ixn.COMMENT_DATE
+        INSERT INTO dbo.D_INTERVIEW_NOTE (D_INTERVIEW_KEY,
+                                          D_INTERVIEW_NOTE_KEY,
+                                          NBS_ANSWER_UID,
+                                          USER_FIRST_NAME,
+                                          USER_LAST_NAME,
+                                          USER_COMMENT,
+                                          COMMENT_DATE)
+        SELECT lintn.D_INTERVIEW_KEY,
+               lintn.D_INTERVIEW_NOTE_KEY,
+               ixn.NBS_ANSWER_UID,
+               ixn.USER_FIRST_NAME,
+               ixn.USER_LAST_NAME,
+               ixn.USER_COMMENT,
+               ixn.COMMENT_DATE
         FROM #L_INTERVIEW_NOTE_N lintn
-            LEFT JOIN #INTERVIEW_NOTE_INIT ixn
-            ON lintn.NBS_ANSWER_UID = ixn.nbs_answer_uid;
-
+                 LEFT JOIN #INTERVIEW_NOTE_INIT ixn
+                           ON lintn.NBS_ANSWER_UID = ixn.nbs_answer_uid;
 
 
         SELECT @RowCount_no = @@ROWCOUNT;
@@ -536,17 +554,14 @@ BEGIN
             @PROC_STEP_NAME = 'UPDATE D_INTERVIEW_NOTE';
 
         UPDATE din
-        SET 
-            din.USER_FIRST_NAME = ixn.USER_FIRST_NAME,
-            din.USER_LAST_NAME = ixn.USER_LAST_NAME,
-            din.USER_COMMENT = ixn.USER_COMMENT,
-            din.COMMENT_DATE = ixn.COMMENT_DATE
+        SET din.USER_FIRST_NAME = ixn.USER_FIRST_NAME,
+            din.USER_LAST_NAME  = ixn.USER_LAST_NAME,
+            din.USER_COMMENT    = ixn.USER_COMMENT,
+            din.COMMENT_DATE    = ixn.COMMENT_DATE
         FROM #INTERVIEW_NOTE_INIT ixn
-            LEFT JOIN dbo.D_INTERVIEW_NOTE din
-            ON ixn.NBS_ANSWER_UID = din.NBS_ANSWER_UID
+                 LEFT JOIN dbo.D_INTERVIEW_NOTE din
+                           ON ixn.NBS_ANSWER_UID = din.NBS_ANSWER_UID
         WHERE ixn.NBS_ANSWER_UID NOT IN (SELECT NBS_ANSWER_UID FROM #L_INTERVIEW_NOTE_N);
-        
-
 
 
         SELECT @RowCount_no = @@ROWCOUNT;
@@ -557,7 +572,7 @@ BEGIN
 
         COMMIT TRANSACTION;
 
-        
+
     END TRY
     BEGIN CATCH
 
