14f6500254543d7246b1ce1f7c362dcec8e2d92a
UpasanaPattnaik <upattnaik@enquizit.com>
2024-12-03 16:37:53 -0500

    CNDE-1916: Reducing lines of code

--- liquibase-service/src/main/resources/db/rdb_modern/tables/007-create_nrt_investigation-001.sql
+++ liquibase-service/src/main/resources/db/rdb_modern/tables/007-create_nrt_investigation-001.sql
@@ -122,11 +122,6 @@ IF EXISTS (SELECT 1 FROM sysobjects WHERE name = 'nrt_investigation' and xtype =
             ALTER TABLE dbo.nrt_investigation ADD investigation_form_cd VARCHAR(50);
             END;
 
-    END;
-
-IF EXISTS (SELECT 1 FROM sysobjects WHERE name = 'nrt_investigation' and xtype = 'U')
-    BEGIN
-
         IF NOT EXISTS(SELECT 1 FROM sys.columns WHERE name = N'outbreak_name_desc' AND Object_ID = Object_ID(N'nrt_investigation'))
             BEGIN
                 ALTER TABLE dbo.nrt_investigation ADD outbreak_name_desc VARCHAR(100);
