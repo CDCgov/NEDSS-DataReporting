36858d07a8efdff98de634493e1d1e5fd02884c6
Sergei Veselev <sveselev@enquizit.com>
2024-12-12 14:15:00 -0500

    CNDE-1923 Liquibase clean up

--- liquibase-service/src/main/resources/db/rdb_modern/tables/022-create_nrt_interview_note-001.sql
+++ liquibase-service/src/main/resources/db/rdb_modern/tables/022-create_nrt_interview_note-001.sql
@@ -15,3 +15,15 @@ CREATE TABLE dbo.nrt_interview_note
     PERIOD FOR SYSTEM_TIME (refresh_datetime, max_datetime)
 );
 
+IF EXISTS (SELECT 1 FROM sysobjects WHERE name = 'nrt_interview_note' and xtype = 'U')
+    BEGIN
+
+--CNDE-1921
+        IF NOT EXISTS(SELECT 1 FROM sys.columns   WHERE Name = N'record_status_cd'   AND Object_ID = Object_ID(N'nrt_interview_note'))
+            BEGIN
+                ALTER TABLE nrt_interview_note
+                    ADD record_status_cd varchar(4000);
+            END;
+
+    END;
+
