e4a07084e96245165474c43db103f29c602441a2
Ariel Gonzalez <agonzalez@ignw.io>
2025-03-20 16:18:55 -0400

    fixing tests

--- post-processing-service/src/test/java/gov/cdc/etldatapipeline/postprocessingservice/service/PostProcessingServiceTest.java
+++ post-processing-service/src/test/java/gov/cdc/etldatapipeline/postprocessingservice/service/PostProcessingServiceTest.java
@@ -164,7 +164,7 @@ class PostProcessingServiceTest {
 
 
         List<ILoggingEvent> logs = listAppender.list;
-        assertEquals(18, logs.size());
+        assertEquals(20, logs.size());
         assertTrue(logs.get(2).getFormattedMessage().contains(INVESTIGATION.getStoredProcedure()));
         assertTrue(logs.get(5).getMessage().contains(PostProcessingService.SP_EXECUTION_COMPLETED));
     }
@@ -184,7 +184,7 @@ class PostProcessingServiceTest {
         verify(investigationRepositoryMock, never()).executeStoredProcForPageBuilder(anyLong(), anyString());
 
         List<ILoggingEvent> logs = listAppender.list;
-        assertEquals(20, logs.size());
+        assertEquals(22, logs.size());
         assertTrue(logs.get(2).getFormattedMessage().contains(INVESTIGATION.getStoredProcedure()));
         assertTrue(logs.get(5).getMessage().contains(PostProcessingService.SP_EXECUTION_COMPLETED));
     }
@@ -202,7 +202,7 @@ class PostProcessingServiceTest {
         verify(investigationRepositoryMock).executeStoredProcForAggregateReport(expectedPublicHealthCaseIdsString);
 
         List<ILoggingEvent> logs = listAppender.list;
-        assertEquals(22, logs.size());
+        assertEquals(20, logs.size());
         assertTrue(logs.get(2).getFormattedMessage().contains(INVESTIGATION.getStoredProcedure()));
         assertTrue(logs.get(5).getMessage().contains(PostProcessingService.SP_EXECUTION_COMPLETED));
     }
@@ -292,7 +292,7 @@ class PostProcessingServiceTest {
                 expectedRdbTableNames);
 
         List<ILoggingEvent> logs = listAppender.list;
-        assertEquals(24, logs.size());
+        assertEquals(18, logs.size());
         assertTrue(logs.get(7).getMessage().contains(PostProcessingService.SP_EXECUTION_COMPLETED));
     }
 
@@ -589,18 +589,19 @@ class PostProcessingServiceTest {
         assertTrue(topicLogList.get(9).contains(invTopic));
         assertTrue(topicLogList.get(10).contains(invTopic));
         assertTrue(topicLogList.get(11).contains(invTopic));
-        assertTrue(topicLogList.get(12).contains(ntfTopic));
-        assertTrue(topicLogList.get(13).contains(treatmentTopic));
-        assertTrue(topicLogList.get(14).contains(intTopic));
+        assertTrue(topicLogList.get(12).contains(invTopic));
+        assertTrue(topicLogList.get(13).contains(ntfTopic));
+        assertTrue(topicLogList.get(14).contains(treatmentTopic));
         assertTrue(topicLogList.get(15).contains(intTopic));
-        assertTrue(topicLogList.get(16).contains(cmTopic));
+        assertTrue(topicLogList.get(16).contains(intTopic));
         assertTrue(topicLogList.get(17).contains(cmTopic));
-        assertTrue(topicLogList.get(18).contains(ldfTopic));
-        assertTrue(topicLogList.get(19).contains(obsTopic));
-        assertTrue(topicLogList.get(20).contains(contactTopic));
+        assertTrue(topicLogList.get(18).contains(cmTopic));
+        assertTrue(topicLogList.get(19).contains(ldfTopic));
+        assertTrue(topicLogList.get(20).contains(obsTopic));
         assertTrue(topicLogList.get(21).contains(contactTopic));
-        assertTrue(topicLogList.get(22).contains(vacTopic));
+        assertTrue(topicLogList.get(22).contains(contactTopic));
         assertTrue(topicLogList.get(23).contains(vacTopic));
+        assertTrue(topicLogList.get(24).contains(vacTopic));
     }
 
     @Test
