a57fe35637854a90c378f0b543d863af4ac43109
Ariel Gonzalez <agonzalez@ignw.io>
2025-03-24 17:45:07 -0400

    renaming @Dataflow_name to have a similar names

--- liquibase-service/src/main/resources/db/rdb_modern/routines/200-sp_nrt_d_smr_exam_ty_postprocessing-001.sql
+++ liquibase-service/src/main/resources/db/rdb_modern/routines/200-sp_nrt_d_smr_exam_ty_postprocessing-001.sql
@@ -10,7 +10,7 @@ BEGIN
     DECLARE @RowCount_no INT;
     DECLARE @Proc_Step_no FLOAT= 0;
     DECLARE @Proc_Step_Name VARCHAR(200)= '';
-	DECLARE @Dataflow_Name VARCHAR(200) = 'D_SMR_EXAM_TY Post-Processing Event';
+	DECLARE @Dataflow_Name VARCHAR(200) = 'D_SMR_EXAM_TY POST-Processing';
 	DECLARE @Package_Name VARCHAR(200) = 'sp_nrt_d_smr_exam_ty_postprocessing';
 
     BEGIN TRY
