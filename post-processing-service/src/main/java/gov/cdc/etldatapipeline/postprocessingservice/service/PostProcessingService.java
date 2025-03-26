533c58fa8a0ac4bac6571bdc8710777f689a6c2a
Ariel Gonzalez <agonzalez@ignw.io>
2025-03-26 17:24:31 -0400

    wip

--- post-processing-service/src/main/java/gov/cdc/etldatapipeline/postprocessingservice/service/PostProcessingService.java
+++ post-processing-service/src/main/java/gov/cdc/etldatapipeline/postprocessingservice/service/PostProcessingService.java
@@ -384,7 +384,7 @@ public class PostProcessingService {
         processTopic(keyTopic, TB_PAM_LDF, ids, investigationRepository::executeStoredProcForTbPamLdf);
         processTopic(keyTopic, D_VAR_PAM, ids, investigationRepository::executeStoredProcForDVarPam);
         processTopic(keyTopic, D_PCR_SOURCE, ids, investigationRepository::executeStoredProcForDPcrSource);
-        
+         
         return dmData;
     }
 
