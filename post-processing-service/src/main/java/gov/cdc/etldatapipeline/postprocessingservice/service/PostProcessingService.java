bc7fa3fc7fa86407315a76c960911a475e3e14ae
Ariel Gonzalez <agonzalez@ignw.io>
2025-04-25 12:25:17 -0400

    Adding condition to execute LDFs based on featureflag ldfEnable

--- post-processing-service/src/main/java/gov/cdc/etldatapipeline/postprocessingservice/service/PostProcessingService.java
+++ post-processing-service/src/main/java/gov/cdc/etldatapipeline/postprocessingservice/service/PostProcessingService.java
@@ -570,26 +570,28 @@ public class PostProcessingService {
                     case GENERIC_CASE:
                         executeDatamartProc(GENERIC_CASE,
                             investigationRepository::executeStoredProcForGenericCaseDatamart, cases);
-                            
-                        switch (Entity.valueOf(ldfType.toUpperCase())) {
-                            case LDF_GENERIC:
-                                executeDatamartProc(LDF_GENERIC,
-                                investigationRepository::executeStoredProcForLdfGenericDatamart, cases);
-                                break;
-                            case LDF_FOODBORNE:
-                                executeDatamartProc(LDF_FOODBORNE,
-                                investigationRepository::executeStoredProcForLdfFoodBorneDatamart, cases);
-                                break;
-                            case LDF_TETANUS:
-                                // to test this Tetanus must be added as a legacy page 
-                                // and [dbo].nrt_datamart_metadata table must be updated
-                                // adding TETANUS
-                                executeDatamartProc(LDF_TETANUS,
-                                investigationRepository::executeStoredProcForLdfTetanusDatamart, cases);
-                                break;
-                            default:
-                                break;
-                        }
+                        
+                        if (ldfEnable){
+                            switch (Entity.valueOf(ldfType.toUpperCase())) {
+                                case LDF_GENERIC:
+                                    executeDatamartProc(LDF_GENERIC,
+                                    investigationRepository::executeStoredProcForLdfGenericDatamart, cases);
+                                    break;
+                                case LDF_FOODBORNE:
+                                    executeDatamartProc(LDF_FOODBORNE,
+                                    investigationRepository::executeStoredProcForLdfFoodBorneDatamart, cases);
+                                    break;
+                                case LDF_TETANUS:
+                                    // to test this, Tetanus must be added as a legacy page and 
+                                    // [dbo].nrt_datamart_metadata table must be updated adding TETANUS
+                                    executeDatamartProc(LDF_TETANUS,
+                                    investigationRepository::executeStoredProcForLdfTetanusDatamart, cases);
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }    
+                        
                         break;
                     case CRS_CASE:
                         executeDatamartProc(CRS_CASE,
@@ -611,7 +613,7 @@ public class PostProcessingService {
                         executeDatamartProc(BMIRD_CASE,
                                 investigationRepository::executeStoredProcForBmirdCaseDatamart, cases);
                         
-                        if(ldfType.equalsIgnoreCase("LDF_BMIRD")){
+                        if(ldfEnable && ldfType.equalsIgnoreCase("LDF_BMIRD")){
                             executeDatamartProc(LDF_BMIRD,
                             investigationRepository::executeStoredProcForLdfBmirdDatamart, cases);
                         }
