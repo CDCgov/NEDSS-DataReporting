8f774fbb76d7c1390e3a6f6bffc66d49f3f745a6
agonzalez-cloud39 <50598734+agonzalez-cloud39@users.noreply.github.com>
2025-05-13 13:43:00 -0400

    CNDE-2578 Modifying all metadata views views to use rdb_modern database instead of NBS_ODSE database (#418)
    Co-authored-by: Ariel Gonzalez <agonzalez@ignw.io>

--- db/upgrade/rdb_modern/views/017-v_nrt_d_inv_metadata.sql
+++ db/upgrade/rdb_modern/views/017-v_nrt_d_inv_metadata.sql
@@ -1,23 +1,22 @@
 CREATE OR ALTER VIEW dbo.v_nrt_d_inv_metadata AS
-SELECT DISTINCT NBS_UI_METADATA.INVESTIGATION_FORM_CD
-              , page.FORM_CD
-              , page.DATAMART_NM
-              , NBS_RDB_METADATA.RDB_TABLE_NM
-              , NBS_RDB_METADATA.RDB_COLUMN_NM
-              , NBS_RDB_METADATA.USER_DEFINED_COLUMN_NM
-              , OTHER_VALUE_IND_CD
-              , data_type
-              , CODE_SET_GROUP_ID
-              , mask
-              , UNIT_TYPE_CD
--- into #tmp_DynDm_D_INV_METADATA
-FROM dbo.v_nrt_nbs_page page with (nolock)
-         INNER JOIN NBS_ODSE..NBS_UI_METADATA with (nolock) ON NBS_UI_METADATA.INVESTIGATION_FORM_CD = page.FORM_CD
-         INNER JOIN NBS_ODSE..NBS_RDB_METADATA with (nolock)
-                    ON NBS_UI_METADATA.NBS_UI_METADATA_UID = NBS_RDB_METADATA.NBS_UI_METADATA_UID
-WHERE QUESTION_GROUP_SEQ_NBR IS NULL
-  AND NBS_RDB_METADATA.USER_DEFINED_COLUMN_NM <> ''
-  and NBS_RDB_METADATA.USER_DEFINED_COLUMN_NM IS NOT NULL
--- and RDB_TABLE_NM=@RDB_TABLE_NM AND
--- and NBS_UI_METADATA.INVESTIGATION_FORM_CD=(SELECT distinct FORM_CD FROM dbo.v_nrt_nbs_page with (nolock) WHERE DATAMART_NM= @DATAMART_NAME )
-;
+SELECT DISTINCT 
+	ui_meta.INVESTIGATION_FORM_CD,
+  	page.FORM_CD,
+  	page.DATAMART_NM,
+  	rdb_meta.RDB_TABLE_NM,
+  	rdb_meta.RDB_COLUMN_NM,
+  	rdb_meta.USER_DEFINED_COLUMN_NM,
+  	OTHER_VALUE_IND_CD,
+  	data_type,
+  	CODE_SET_GROUP_ID,
+  	mask,
+  	UNIT_TYPE_CD
+FROM dbo.v_nrt_nbs_page page WITH(NOLOCK)
+INNER JOIN [dbo].nrt_odse_NBS_ui_metadata ui_meta WITH(NOLOCK) 
+	ON ui_meta.INVESTIGATION_FORM_CD = page.FORM_CD
+INNER JOIN [dbo].nrt_odse_NBS_rdb_metadata rdb_meta WITH(NOLOCK)
+  ON ui_meta.NBS_UI_METADATA_UID = rdb_meta.NBS_UI_METADATA_UID
+WHERE 
+	QUESTION_GROUP_SEQ_NBR IS NULL
+	AND rdb_meta.USER_DEFINED_COLUMN_NM <> ''
+	AND rdb_meta.USER_DEFINED_COLUMN_NM IS NOT NULL;
