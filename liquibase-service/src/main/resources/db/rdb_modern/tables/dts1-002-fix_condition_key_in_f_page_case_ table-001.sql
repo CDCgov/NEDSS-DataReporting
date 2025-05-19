5cdcc8d0c8848059b65705c57e7cf62e3a97b558
Ariel Gonzalez <agonzalez@ignw.io>
2025-05-19 19:20:39 -0400

    Working in ticket CNDE-2500

---
+++ liquibase-service/src/main/resources/db/rdb_modern/tables/dts1-002-fix_condition_key_in_f_page_case_ table-001.sql
@@ -0,0 +1,17 @@
+IF  EXISTS (SELECT * FROM rdb_modern.INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND  TABLE_NAME = 'INVESTIGATION')
+	AND EXISTS (SELECT * FROM rdb_modern.INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND  TABLE_NAME = 'F_PAGE_CASE')
+	AND EXISTS (select * FROM rdb_modern.sys.views where name = 'v_condition_dim')
+	AND EXISTS (SELECT * FROM nbs_odse.INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND  TABLE_NAME = 'public_health_case')
+	AND EXISTS (SELECT * FROM nbs_srte.INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND  TABLE_NAME = 'Condition_code')
+
+BEGIN
+	UPDATE f
+		SET CONDITION_KEY = (SELECT TOP(1) condition_key FROM [rdb_modern].[dbo].v_condition_dim WITH (NOLOCK) WHERE condition_cd = cc2.condition_cd)
+	FROM [rdb_modern].[dbo].INVESTIGATION inv WITH (NOLOCK) 
+		INNER JOIN [nbs_odse].[dbo].public_health_case phc WITH (NOLOCK) ON phc.public_health_case_uid = inv.CASE_UID 
+		INNER JOIN [nbs_srte].[dbo].Condition_code cc2 WITH (NOLOCK) ON cc2.condition_cd = phc.cd
+		INNER JOIN [rdb_modern].[dbo].F_PAGE_CASE f WITH (NOLOCK) ON f.INVESTIGATION_KEY = inv.INVESTIGATION_KEY
+		INNER JOIN [rdb_modern].[dbo].v_condition_dim vc WITH (NOLOCK) ON f.CONDITION_KEY = vc.CONDITION_KEY
+	WHERE 
+		COALESCE(cc2.condition_cd,'') <> COALESCE(vc.condition_cd,'')
+END
