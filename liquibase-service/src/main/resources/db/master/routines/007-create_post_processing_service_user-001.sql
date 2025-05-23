6e2250beea5688dfa5f1b3fdc27d4d3214325b0b
Jayasudha Sundaram <jayasudha.sundaram@cdwg.com>
2025-05-23 12:01:30 -0400

    fixed the regexp issue in postprocessing service

--- liquibase-service/src/main/resources/db/master/routines/007-create_post_processing_service_user-001.sql
+++ liquibase-service/src/main/resources/db/master/routines/007-create_post_processing_service_user-001.sql
@@ -12,55 +12,58 @@ PRINT 'Switched to database [rdb_modern]';
 
 -- Check if user exists in this database
 IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = @UserName)
-    BEGIN
-        -- Create the user in RDB_MODERN database
-        DECLARE @CreateUserRDBModernSQL NVARCHAR(MAX) = 'CREATE USER [' + @UserName + '] FOR LOGIN [' + @UserName + ']';
-        EXEC sp_executesql @CreateUserRDBModernSQL;
-        PRINT 'Created database user [' + @UserName + '] in rdb_modern';
+BEGIN
+    -- Create the user in RDB_MODERN database
+    DECLARE @CreateUserRDBModernSQL NVARCHAR(MAX) = 'CREATE USER [' + @UserName + '] FOR LOGIN [' + @UserName + ']';
+    EXEC sp_executesql @CreateUserRDBModernSQL;
+    PRINT 'Created database user [' + @UserName + '] in rdb_modern';
 
-        -- Grant CONNECT permission
-        DECLARE @GrantConnectRDBModernSQL NVARCHAR(MAX) = 'GRANT CONNECT TO [' + @UserName + ']';
-        EXEC sp_executesql @GrantConnectRDBModernSQL;
-        PRINT 'Granted CONNECT permission to [' + @UserName + '] in rdb_modern';
+    -- Grant CONNECT permission
+    DECLARE @GrantConnectRDBModernSQL NVARCHAR(MAX) = 'GRANT CONNECT TO [' + @UserName + ']';
+    EXEC sp_executesql @GrantConnectRDBModernSQL;
+    PRINT 'Granted CONNECT permission to [' + @UserName + '] in rdb_modern';
 
-        -- Grant data reader role
-        DECLARE @AddRoleMemberRDBModernReaderSQL NVARCHAR(MAX) = 'EXEC sp_addrolemember ''db_datareader'', ''' + @UserName + '''';
-        EXEC sp_executesql @AddRoleMemberRDBModernReaderSQL;
-        PRINT 'Added [' + @UserName + '] to db_datareader role in rdb_modern';
+    -- Grant data reader role
+    DECLARE @AddRoleMemberRDBModernReaderSQL NVARCHAR(MAX) = 'EXEC sp_addrolemember ''db_datareader'', ''' + @UserName + '''';
+    EXEC sp_executesql @AddRoleMemberRDBModernReaderSQL;
+    PRINT 'Added [' + @UserName + '] to db_datareader role in rdb_modern';
 
-        -- Grant data writer role
-        DECLARE @AddRoleMemberRDBModernWriterSQL NVARCHAR(MAX) = 'EXEC sp_addrolemember ''db_datawriter'', ''' + @UserName + '''';
-        EXEC sp_executesql @AddRoleMemberRDBModernWriterSQL;
-        PRINT 'Added [' + @UserName + '] to db_datawriter role in rdb_modern';
-    END
+    -- Grant data writer role
+    DECLARE @AddRoleMemberRDBModernWriterSQL NVARCHAR(MAX) = 'EXEC sp_addrolemember ''db_datawriter'', ''' + @UserName + '''';
+    EXEC sp_executesql @AddRoleMemberRDBModernWriterSQL;
+    PRINT 'Added [' + @UserName + '] to db_datawriter role in rdb_modern';
+END
 ELSE
-    BEGIN
-        PRINT 'User [' + @UserName + '] already exists in rdb_modern';
+BEGIN
+    PRINT 'User [' + @UserName + '] already exists in rdb_modern';
 
-        -- Grant CONNECT permission for existing user
-        DECLARE @GrantConnectRDBModernExistingSQL NVARCHAR(MAX) = 'GRANT CONNECT TO [' + @UserName + ']';
-        EXEC sp_executesql @GrantConnectRDBModernExistingSQL;
-        PRINT 'Granted CONNECT permission to [' + @UserName + '] in rdb_modern';
+    -- Grant CONNECT permission for existing user
+    DECLARE @GrantConnectRDBModernExistingSQL NVARCHAR(MAX) = 'GRANT CONNECT TO [' + @UserName + ']';
+    EXEC sp_executesql @GrantConnectRDBModernExistingSQL;
+    PRINT 'Granted CONNECT permission to [' + @UserName + '] in rdb_modern';
 
-        -- Grant data reader role for existing user
-        DECLARE @AddRoleMemberRDBModernReaderExistingSQL NVARCHAR(MAX) = 'EXEC sp_addrolemember ''db_datareader'', ''' + @UserName + '''';
-        EXEC sp_executesql @AddRoleMemberRDBModernReaderExistingSQL;
-        PRINT 'Added [' + @UserName + '] to db_datareader role in rdb_modern';
+    -- Grant data reader role for existing user
+    DECLARE @AddRoleMemberRDBModernReaderExistingSQL NVARCHAR(MAX) = 'EXEC sp_addrolemember ''db_datareader'', ''' + @UserName + '''';
+    EXEC sp_executesql @AddRoleMemberRDBModernReaderExistingSQL;
+    PRINT 'Added [' + @UserName + '] to db_datareader role in rdb_modern';
 
-        -- Grant data writer role for existing user
-        DECLARE @AddRoleMemberRDBModernWriterExistingSQL NVARCHAR(MAX) = 'EXEC sp_addrolemember ''db_datawriter'', ''' + @UserName + '''';
-        EXEC sp_executesql @AddRoleMemberRDBModernWriterExistingSQL;
-        PRINT 'Added [' + @UserName + '] to db_datawriter role in rdb_modern';
-    END
+    -- Grant data writer role for existing user
+    DECLARE @AddRoleMemberRDBModernWriterExistingSQL NVARCHAR(MAX) = 'EXEC sp_addrolemember ''db_datawriter'', ''' + @UserName + '''';
+    EXEC sp_executesql @AddRoleMemberRDBModernWriterExistingSQL;
+    PRINT 'Added [' + @UserName + '] to db_datawriter role in rdb_modern';
+END
 
 -- Grant EXECUTE permission on all post-processing stored procedures (always grant regardless of user status)
 DECLARE @GrantExecSQL NVARCHAR(MAX) =
-    'DECLARE @sql NVARCHAR(MAX) = '''';
-     SELECT @sql = @sql + ''GRANT EXECUTE ON ['' + SCHEMA_NAME(schema_id) + ''].['' + name + ''] TO [' + @UserName + '];'' + CHAR(13)
-     FROM sys.procedures
-     WHERE name LIKE ''sp_%\\_postprocessing%'' ESCAPE ''\'' OR name LIKE ''sp_%\\_datamart%'' ESCAPE ''\'' OR name LIKE ''sp_nrt_%'' ESCAPE ''\'' OR name LIKE ''sp_f_%'' ESCAPE ''\'' OR name LIKE ''sp_d_%'' ESCAPE ''\''
-     EXEC sp_executesql @sql;';
-
+'DECLARE @sql NVARCHAR(MAX) = '''';
+SELECT @sql = @sql + ''GRANT EXECUTE ON ['' + SCHEMA_NAME(schema_id) + ''].['' + name + ''] TO [' + @UserName + '];'' + CHAR(13)
+FROM sys.procedures
+WHERE name LIKE ''sp_%_postprocessing%''
+   OR name LIKE ''sp_%_datamart%''
+   OR name LIKE ''sp_nrt_%''
+   OR name LIKE ''sp_f_%''
+   OR name LIKE ''sp_d_%''
+ EXEC sp_executesql @sql;';
 EXEC sp_executesql @GrantExecSQL;
 PRINT 'Granted EXECUTE permission on all post-processing stored procedures to [' + @UserName + ']';
 
