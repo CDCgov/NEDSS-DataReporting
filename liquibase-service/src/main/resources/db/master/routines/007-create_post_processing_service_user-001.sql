bd8a7c66f3ddbff1cc14a249e33665fb56d456e8
Jayasudha Sundaram <jayasudha.sundaram@cdwg.com>
2025-05-23 13:40:39 -0400

    adding fix for post processing

--- liquibase-service/src/main/resources/db/master/routines/007-create_post_processing_service_user-001.sql
+++ liquibase-service/src/main/resources/db/master/routines/007-create_post_processing_service_user-001.sql
@@ -1,11 +1,50 @@
 -- SQL Script to create service-specific user for post-processing-service
--- This script creates a dedicated user and grants necessary permissions
+-- This script creates a dedicated user and grants db_owner permissions on both databases
 
 DECLARE @ServiceName NVARCHAR(100) = 'post_processing_service';
 DECLARE @UserName NVARCHAR(150) = @ServiceName + '_rdb';
 
 -- ==========================================
--- Grant permissions on RDB_modern database (READ/WRITE)
+-- Grant permissions on RDB database (DB_OWNER)
+-- ==========================================
+USE [rdb];
+PRINT 'Switched to database [rdb]';
+
+-- Check if user exists in this database
+IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = @UserName)
+BEGIN
+    -- Create the user in RDB database
+    DECLARE @CreateUserRDBSQL NVARCHAR(MAX) = 'CREATE USER [' + @UserName + '] FOR LOGIN [' + @UserName + ']';
+    EXEC sp_executesql @CreateUserRDBSQL;
+    PRINT 'Created database user [' + @UserName + '] in rdb';
+
+    -- Grant CONNECT permission
+    DECLARE @GrantConnectRDBSQL NVARCHAR(MAX) = 'GRANT CONNECT TO [' + @UserName + ']';
+    EXEC sp_executesql @GrantConnectRDBSQL;
+    PRINT 'Granted CONNECT permission to [' + @UserName + '] in rdb';
+
+    -- Grant db_owner role
+    DECLARE @AddRoleMemberRDBOwnerSQL NVARCHAR(MAX) = 'EXEC sp_addrolemember ''db_owner'', ''' + @UserName + '''';
+    EXEC sp_executesql @AddRoleMemberRDBOwnerSQL;
+    PRINT 'Added [' + @UserName + '] to db_owner role in rdb';
+END
+ELSE
+BEGIN
+    PRINT 'User [' + @UserName + '] already exists in rdb';
+
+    -- Grant CONNECT permission for existing user
+    DECLARE @GrantConnectRDBExistingSQL NVARCHAR(MAX) = 'GRANT CONNECT TO [' + @UserName + ']';
+    EXEC sp_executesql @GrantConnectRDBExistingSQL;
+    PRINT 'Granted CONNECT permission to [' + @UserName + '] in rdb';
+
+    -- Grant db_owner role for existing user
+    DECLARE @AddRoleMemberRDBOwnerExistingSQL NVARCHAR(MAX) = 'EXEC sp_addrolemember ''db_owner'', ''' + @UserName + '''';
+    EXEC sp_executesql @AddRoleMemberRDBOwnerExistingSQL;
+    PRINT 'Added [' + @UserName + '] to db_owner role in rdb';
+END
+
+-- ==========================================
+-- Grant permissions on RDB_modern database (DB_OWNER)
 -- ==========================================
 USE [rdb_modern];
 PRINT 'Switched to database [rdb_modern]';
@@ -23,15 +62,10 @@ BEGIN
     EXEC sp_executesql @GrantConnectRDBModernSQL;
     PRINT 'Granted CONNECT permission to [' + @UserName + '] in rdb_modern';
 
-    -- Grant data reader role
-    DECLARE @AddRoleMemberRDBModernReaderSQL NVARCHAR(MAX) = 'EXEC sp_addrolemember ''db_datareader'', ''' + @UserName + '''';
-    EXEC sp_executesql @AddRoleMemberRDBModernReaderSQL;
-    PRINT 'Added [' + @UserName + '] to db_datareader role in rdb_modern';
-
-    -- Grant data writer role
-    DECLARE @AddRoleMemberRDBModernWriterSQL NVARCHAR(MAX) = 'EXEC sp_addrolemember ''db_datawriter'', ''' + @UserName + '''';
-    EXEC sp_executesql @AddRoleMemberRDBModernWriterSQL;
-    PRINT 'Added [' + @UserName + '] to db_datawriter role in rdb_modern';
+    -- Grant db_owner role
+    DECLARE @AddRoleMemberRDBModernOwnerSQL NVARCHAR(MAX) = 'EXEC sp_addrolemember ''db_owner'', ''' + @UserName + '''';
+    EXEC sp_executesql @AddRoleMemberRDBModernOwnerSQL;
+    PRINT 'Added [' + @UserName + '] to db_owner role in rdb_modern';
 END
 ELSE
 BEGIN
@@ -42,32 +76,15 @@ BEGIN
     EXEC sp_executesql @GrantConnectRDBModernExistingSQL;
     PRINT 'Granted CONNECT permission to [' + @UserName + '] in rdb_modern';
 
-    -- Grant data reader role for existing user
-    DECLARE @AddRoleMemberRDBModernReaderExistingSQL NVARCHAR(MAX) = 'EXEC sp_addrolemember ''db_datareader'', ''' + @UserName + '''';
-    EXEC sp_executesql @AddRoleMemberRDBModernReaderExistingSQL;
-    PRINT 'Added [' + @UserName + '] to db_datareader role in rdb_modern';
-
-    -- Grant data writer role for existing user
-    DECLARE @AddRoleMemberRDBModernWriterExistingSQL NVARCHAR(MAX) = 'EXEC sp_addrolemember ''db_datawriter'', ''' + @UserName + '''';
-    EXEC sp_executesql @AddRoleMemberRDBModernWriterExistingSQL;
-    PRINT 'Added [' + @UserName + '] to db_datawriter role in rdb_modern';
+    -- Grant db_owner role for existing user
+    DECLARE @AddRoleMemberRDBModernOwnerExistingSQL NVARCHAR(MAX) = 'EXEC sp_addrolemember ''db_owner'', ''' + @UserName + '''';
+    EXEC sp_executesql @AddRoleMemberRDBModernOwnerExistingSQL;
+    PRINT 'Added [' + @UserName + '] to db_owner role in rdb_modern';
 END
-
--- Grant EXECUTE permission on all post-processing stored procedures (always grant regardless of user status)
-DECLARE @GrantExecSQL NVARCHAR(MAX) =
-'DECLARE @sql NVARCHAR(MAX) = '''';
-SELECT @sql = @sql + ''GRANT EXECUTE ON ['' + SCHEMA_NAME(schema_id) + ''].['' + name + ''] TO [' + @UserName + '];'' + CHAR(13)
-FROM sys.procedures
-WHERE name LIKE ''sp_%_postprocessing%''
-   OR name LIKE ''sp_%_datamart%''
-   OR name LIKE ''sp_nrt_%''
-   OR name LIKE ''sp_f_%''
-   OR name LIKE ''sp_d_%''
- EXEC sp_executesql @sql;';
-EXEC sp_executesql @GrantExecSQL;
-PRINT 'Granted EXECUTE permission on all post-processing stored procedures to [' + @UserName + ']';
 
 -- ==========================================
 -- Verify permissions for the new user
 -- ==========================================
-PRINT 'User creation completed. Verify that the user has been created in the database and has the correct permissions.';
+PRINT 'User creation completed with db_owner permissions on both rdb and rdb_modern databases.';
+PRINT 'The post-processing service user now has full permissions to create/alter tables, columns, indexes, functions, and views.';
+PRINT 'Verify that the user has been created in both databases and has the correct permissions.';
