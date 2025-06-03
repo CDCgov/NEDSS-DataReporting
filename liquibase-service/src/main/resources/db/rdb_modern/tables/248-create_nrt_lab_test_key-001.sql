4840461bc001a6b2da72db0f68fc9cb8b2376a69
Ariel Gonzalez <agonzalez@ignw.io>
2025-06-03 09:58:16 -0400

    adding table create to changelog

--- liquibase-service/src/main/resources/db/rdb_modern/tables/248-create_nrt_lab_test_key-001.sql
+++ liquibase-service/src/main/resources/db/rdb_modern/tables/248-create_nrt_lab_test_key-001.sql
@@ -1,15 +1,23 @@
-  IF NOT EXISTS (SELECT 1 FROM sysobjects WHERE name = 'nrt_lab_test_key' and xtype = 'U')
-    BEGIN
+IF NOT EXISTS (SELECT 1 FROM sysobjects WHERE name = 'nrt_lab_test_key' and xtype = 'U')
+	BEGIN
 
-        CREATE TABLE [dbo].nrt_lab_test_key (
-          LAB_TEST_KEY bigint IDENTITY(1,1) NOT NULL,
-          LAB_TEST_UID bigint NULL,
-          created_dttm DATETIME2 default getdate(),
-          updated_dttm DATETIME2 default getdate()           
-        );
+		CREATE TABLE [dbo].nrt_lab_test_key (
+		LAB_TEST_KEY BIGINT IDENTITY(1,1) NOT NULL,
+		LAB_TEST_UID BIGINT NULL,
+		created_dttm DATETIME2 DEFAULT getdate(),
+		updated_dttm DATETIME2 DEFAULT getdate()           
+		);
 
-        -- Insert Key = 1 with LAB_TEST_UID = NULL
-        INSERT INTO [dbo].nrt_lab_test_key(LAB_TEST_UID)
-        VALUES (NULL)
+		-- Insert Key = 1 with LAB_TEST_UID = NULL
+		INSERT INTO [dbo].nrt_lab_test_key(LAB_TEST_UID)
+		VALUES (NULL)
 
-    END
+	END
+
+--RESEED [dbo].nrt_lab_test_key table 
+DECLARE @max BIGINT;
+SELECT @max=max(LAB_TEST_KEY) from [dbo].LAB_TEST;
+SELECT @max;
+IF @max IS NULL   --check when max is returned as null
+	SET @max = 2; -- default to 2, default record with key = 1 is already created
+DBCC CHECKIDENT ('[dbo].nrt_lab_test_key', RESEED, @max);
--- liquibase-service/src/main/resources/db/rdb_modern/tables/249-create_nrt_lab_rpt_user_comment_key-001.sql
+++ liquibase-service/src/main/resources/db/rdb_modern/tables/249-create_nrt_lab_rpt_user_comment_key-001.sql
@@ -1,16 +1,24 @@
-  IF NOT EXISTS (SELECT 1 FROM sysobjects WHERE name = 'nrt_lab_rpt_user_comment_key' and xtype = 'U')
-    BEGIN
+IF NOT EXISTS (SELECT 1 FROM sysobjects WHERE name = 'nrt_lab_rpt_user_comment_key' and xtype = 'U')
+	BEGIN
 
-        CREATE TABLE [dbo].nrt_lab_rpt_user_comment_key (
-          USER_COMMENT_KEY bigint IDENTITY(1,1) NOT NULL,
-          LAB_TEST_KEY bigint NULL,
-          USER_COMMENT_UID bigint NULL,
-          created_dttm DATETIME2 default getdate(),
-          updated_dttm DATETIME2 default getdate()
-        );
+		CREATE TABLE [dbo].nrt_lab_rpt_user_comment_key (
+			USER_COMMENT_KEY BIGINT IDENTITY(1,1) NOT NULL,
+			LAB_TEST_KEY BIGINT NULL,
+			USER_COMMENT_UID BIGINT NULL,
+			created_dttm DATETIME2 DEFAULT GETDATE(),
+			updated_dttm DATETIME2 DEFAULT GETDATE()
+		);
 
-        -- Insert Key = 1 with LAB_TEST_KEY= 1, USER_COMMENT_UID = NULL
-        INSERT INTO [dbo].nrt_lab_rpt_user_comment_key(LAB_TEST_KEY, USER_COMMENT_UID)
-        VALUES (1, NULL)
+		-- Insert Key = 1 with LAB_TEST_KEY= 1, USER_COMMENT_UID = NULL
+		INSERT INTO [dbo].nrt_lab_rpt_user_comment_key(LAB_TEST_KEY, USER_COMMENT_UID)
+		VALUES (1, NULL)
 
-    END
+	END
+
+--RESEED [dbo].nrt_lab_rpt_user_comment_key table 
+DECLARE @max BIGINT;
+SELECT @max=max(USER_COMMENT_KEY) from [dbo].Lab_Rpt_User_Comment;
+SELECT @max;
+IF @max IS NULL   --check when max is returned as null
+	SET @max = 2; -- default to 2, default record with key = 1 is already created
+DBCC CHECKIDENT ('[dbo].nrt_lab_rpt_user_comment_key', RESEED, @max);
