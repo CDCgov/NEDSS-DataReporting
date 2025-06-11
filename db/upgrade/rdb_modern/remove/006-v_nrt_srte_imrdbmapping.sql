4aeb1491ce379c9cdab644864e9c2154756c9638
Ariel Gonzalez <agonzalez@ignw.io>
2025-06-11 14:22:39 -0400

    adding manual scripts to remove unused views

---
+++ db/upgrade/rdb_modern/remove/006-v_nrt_srte_imrdbmapping.sql
@@ -0,0 +1,6 @@
+
+IF EXISTS(SELECT * FROM sys.views WHERE name = 'v_nrt_srte_imrdbmapping')
+BEGIN
+    DROP VIEW [dbo].v_nrt_srte_imrdbmapping
+END
+GO
---
+++ liquibase-service/src/main/resources/db/rdb_modern/remove/008-v_nrt_srte_code_value_general-001.sql
@@ -0,0 +1,6 @@
+
+IF EXISTS(SELECT * FROM sys.views WHERE name = 'v_nrt_srte_code_value_general')
+BEGIN
+    DROP VIEW [dbo].v_nrt_srte_code_value_general
+END
+GO
