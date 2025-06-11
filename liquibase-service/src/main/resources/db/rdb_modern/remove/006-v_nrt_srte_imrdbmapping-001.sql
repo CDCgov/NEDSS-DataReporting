b7c7bcdf786f90c1e6936606fcde5126a2ca7aa5
Ariel Gonzalez <agonzalez@ignw.io>
2025-06-11 14:24:32 -0400

    wip

--- liquibase-service/src/main/resources/db/rdb_modern/remove/006-v_nrt_srte_imrdbmapping-001.sql
+++ liquibase-service/src/main/resources/db/rdb_modern/remove/006-v_nrt_srte_imrdbmapping-001.sql
@@ -3,4 +3,3 @@ IF EXISTS(SELECT * FROM sys.views WHERE name = 'v_nrt_srte_imrdbmapping')
 BEGIN
     DROP VIEW [dbo].v_nrt_srte_imrdbmapping
 END
-GO
--- liquibase-service/src/main/resources/db/rdb_modern/remove/007-v_nrt_srte_codeset-001.sql
+++ liquibase-service/src/main/resources/db/rdb_modern/remove/007-v_nrt_srte_codeset-001.sql
@@ -3,4 +3,3 @@ IF EXISTS(SELECT * FROM sys.views WHERE name = 'v_nrt_srte_codeset')
 BEGIN
     DROP VIEW [dbo].v_nrt_srte_codeset
 END
-GO
