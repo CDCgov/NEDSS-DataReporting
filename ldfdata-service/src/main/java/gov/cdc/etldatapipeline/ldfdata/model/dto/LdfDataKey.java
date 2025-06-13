411fd97482e3ed5d1e5712b85096356252581fae
Varshinee <vvarshinee@gmail.com>
2025-06-13 12:43:44 -0500

    Bug Fix to set Business obj UID as key in LDF service

--- ldfdata-service/src/main/java/gov/cdc/etldatapipeline/ldfdata/model/dto/LdfDataKey.java
+++ ldfdata-service/src/main/java/gov/cdc/etldatapipeline/ldfdata/model/dto/LdfDataKey.java
@@ -1,6 +1,7 @@
 package gov.cdc.etldatapipeline.ldfdata.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
@@ -11,4 +12,8 @@ public class LdfDataKey {
     @NonNull
     @JsonProperty("ldf_uid")
     private Long ldfUid;
+
+    @NonNull
+    @JsonProperty("business_object_uid")
+    private Long busObjUid;
 }
