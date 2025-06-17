ecaccef93fb72e3d40691c3616d86b0cd4a8b166
anand.loganathan@cdwg.com <anand.loganathan@cdwg.com>
2025-06-16 20:17:41 -0700

    Format the db/upgrade 000-create_rtr_admin_user.sql

--- db/upgrade/master/routines/000-create_rtr_admin_user.sql
+++ db/upgrade/master/routines/000-create_rtr_admin_user.sql
@@ -1,140 +1,100 @@
 -- This script to be run outside of Automation as a one time admin user creation
 -- Reset password of the login when the script is run
-USE
-[master]
+USE [master]
 IF NOT EXISTS (SELECT name
                FROM sys.server_principals
                WHERE name = 'db_deploy_admin')
-BEGIN
-        CREATE
-LOGIN [db_deploy_admin] WITH PASSWORD =N'<to_be_reset_later>', DEFAULT_DATABASE = [master], DEFAULT_LANGUAGE = [us_english], CHECK_EXPIRATION = OFF, CHECK_POLICY = OFF;
+    BEGIN
+        CREATE LOGIN [db_deploy_admin] WITH PASSWORD =N'<to_be_reset_later>', DEFAULT_DATABASE = [master], DEFAULT_LANGUAGE = [us_english], CHECK_EXPIRATION = OFF, CHECK_POLICY = OFF;
 
-        ALTER
-SERVER ROLE [setupadmin] ADD MEMBER [db_deploy_admin];
+        ALTER SERVER ROLE [setupadmin] ADD MEMBER [db_deploy_admin];
 
-        ALTER
-SERVER ROLE [processadmin] ADD MEMBER [db_deploy_admin];
+        ALTER SERVER ROLE [processadmin] ADD MEMBER [db_deploy_admin];
 
-        GRANT ALTER
-ANY CREDENTIAL TO [db_deploy_admin];
+        GRANT ALTER ANY CREDENTIAL TO [db_deploy_admin];
 
-        GRANT ALTER
-ANY LOGIN TO [db_deploy_admin];
+        GRANT ALTER ANY LOGIN TO [db_deploy_admin];
 
-        GRANT CREATE
-ANY DATABASE TO [db_deploy_admin];
+        GRANT CREATE ANY DATABASE TO [db_deploy_admin];
 
-        GRANT VIEW
-SERVER STATE TO [db_deploy_admin];
+        GRANT VIEW SERVER STATE TO [db_deploy_admin];
 
-END
+    END
 
-if
-exists (select 1
+if exists (select 1
            from sys.databases
            where name = 'rdsadmin') -- for aws
-begin
-        USE
-msdb;
+    begin
+        USE msdb;
 
-        IF
-NOT EXISTS (SELECT name FROM master.sys.database_principals WHERE name = 'db_deploy_admin')
-            CREATE
-USER [db_deploy_admin] FOR LOGIN [db_deploy_admin] WITH DEFAULT_SCHEMA =[dbo];
+        IF NOT EXISTS (SELECT name FROM master.sys.database_principals WHERE name = 'db_deploy_admin')
+        CREATE USER [db_deploy_admin] FOR LOGIN [db_deploy_admin] WITH DEFAULT_SCHEMA =[dbo];
 
-GRANT EXECUTE ON msdb.dbo.rds_cdc_enable_db TO db_deploy_admin;
+        GRANT EXECUTE ON msdb.dbo.rds_cdc_enable_db TO db_deploy_admin;
 
-GRANT EXECUTE ON msdb.dbo.rds_cdc_disable_db TO db_deploy_admin;
+        GRANT EXECUTE ON msdb.dbo.rds_cdc_disable_db TO db_deploy_admin;
 
-end;
+    end;
 else
-begin
+    begin
         -- azure and onprem
-        ALTER
-SERVER ROLE [sysadmin] ADD MEMBER [db_deploy_admin]
+        ALTER SERVER ROLE [sysadmin] ADD MEMBER [db_deploy_admin]
 
-        CREATE
-USER [db_deploy_admin] FOR LOGIN [db_deploy_admin] WITH DEFAULT_SCHEMA =[dbo]
+        CREATE USER [db_deploy_admin] FOR LOGIN [db_deploy_admin] WITH DEFAULT_SCHEMA =[dbo]
         GRANT EXECUTE ON sys.sp_cdc_enable_db TO db_deploy_admin;
-GRANT EXECUTE ON sys.sp_cdc_disable_db TO db_deploy_admin;
-end;
+        GRANT EXECUTE ON sys.sp_cdc_disable_db TO db_deploy_admin;
+    end;
 
 
-USE
-[RDB];
-
-IF
-NOT EXISTS (SELECT *
-               FROM sys.database_principals
-               WHERE name = 'db_deploy_admin')
-BEGIN
-        CREATE
-USER [db_deploy_admin] FOR LOGIN [db_deploy_admin] WITH DEFAULT_SCHEMA =[dbo]
-        ALTER
-ROLE [db_owner] ADD MEMBER [db_deploy_admin]
-END;
-
-USE
-[rdb_modern];
-
-IF
-NOT EXISTS (SELECT *
-               FROM sys.database_principals
-               WHERE name = 'db_deploy_admin')
-BEGIN
-        CREATE
-USER [db_deploy_admin] FOR LOGIN [db_deploy_admin] WITH DEFAULT_SCHEMA =[dbo]
-
-        ALTER
-ROLE [db_owner] ADD MEMBER [db_deploy_admin]
-END
-
-USE
-[nbs_odse];
-
-
-IF
-NOT EXISTS (SELECT *
-               FROM sys.database_principals
-               WHERE name = 'db_deploy_admin')
-BEGIN
-        CREATE
-USER [db_deploy_admin] FOR LOGIN [db_deploy_admin] WITH DEFAULT_SCHEMA =[dbo]
-
-        ALTER
-ROLE [db_owner] ADD MEMBER [db_deploy_admin]
-END;
-
-USE
-[nbs_srte];
-
-IF
-NOT EXISTS (SELECT *
-               FROM sys.database_principals
-               WHERE name = 'db_deploy_admin')
-BEGIN
-        CREATE
-USER [db_deploy_admin] FOR LOGIN [db_deploy_admin] WITH DEFAULT_SCHEMA =[dbo]
-
-        ALTER
-ROLE [db_owner] ADD MEMBER [db_deploy_admin]
-END;
-
-USE
-[nbs_msgoute]
-
-
+USE [RDB];
 IF NOT EXISTS (SELECT *
                FROM sys.database_principals
                WHERE name = 'db_deploy_admin')
-BEGIN
-        CREATE
-USER [db_deploy_admin] FOR LOGIN [db_deploy_admin] WITH DEFAULT_SCHEMA =[dbo]
+    BEGIN
+        CREATE USER [db_deploy_admin] FOR LOGIN [db_deploy_admin] WITH DEFAULT_SCHEMA =[dbo]
+        ALTER ROLE [db_owner] ADD MEMBER [db_deploy_admin]
+    END;
+
+USE [rdb_modern];
+IF NOT EXISTS (SELECT *
+               FROM sys.database_principals
+               WHERE name = 'db_deploy_admin')
+    BEGIN
+        CREATE USER [db_deploy_admin] FOR LOGIN [db_deploy_admin] WITH DEFAULT_SCHEMA =[dbo]
+
+        ALTER ROLE [db_owner] ADD MEMBER [db_deploy_admin]
+    END
+
+USE [nbs_odse];
+IF NOT EXISTS (SELECT *
+               FROM sys.database_principals
+               WHERE name = 'db_deploy_admin')
+    BEGIN
+        CREATE USER [db_deploy_admin] FOR LOGIN [db_deploy_admin] WITH DEFAULT_SCHEMA =[dbo]
+
+        ALTER ROLE [db_owner] ADD MEMBER [db_deploy_admin]
+    END;
+
+USE [nbs_srte];
+IF NOT EXISTS (SELECT *
+               FROM sys.database_principals
+               WHERE name = 'db_deploy_admin')
+    BEGIN
+        CREATE USER [db_deploy_admin] FOR LOGIN [db_deploy_admin] WITH DEFAULT_SCHEMA =[dbo]
+
+        ALTER ROLE [db_owner] ADD MEMBER [db_deploy_admin]
+    END;
+
+USE [nbs_msgoute]
+IF NOT EXISTS (SELECT *
+               FROM sys.database_principals
+               WHERE name = 'db_deploy_admin')
+    BEGIN
+        CREATE USER [db_deploy_admin] FOR LOGIN [db_deploy_admin] WITH DEFAULT_SCHEMA =[dbo]
 
 
-        ALTER
-ROLE [db_owner] ADD MEMBER [db_deploy_admin]
-end;
+        ALTER ROLE [db_owner] ADD MEMBER [db_deploy_admin]
+    end;
 
 
 -- ALTER LOGIN db_deploy_admin with password ='<your new pass>';
@@ -157,33 +117,33 @@ insert into dbo.Act values(2,2,3);
 insert into dbo.Act values(3,2,3);
 
 */
-use
-nbs_odse;
-if
-exists (select 1
+use nbs_odse;
+if exists (select 1
            from sys.databases
            where name = 'rdsadmin') -- for aws
-begin
+    begin
 
-        if
-not exists (select 1 FROM sys.databases WHERE is_cdc_enabled = 1 and name = 'nbs_odse')
-begin
-exec msdb.dbo.rds_cdc_enable_db 'nbs_odse';
-end;
-end;
+        if not exists (select 1 FROM sys.databases WHERE is_cdc_enabled = 1 and name = 'nbs_odse')
+            begin
+                exec msdb.dbo.rds_cdc_enable_db 'nbs_odse';
+            end;
+    end;
 else
-begin
-exec sys.sp_cdc_enable_db 'nds_odse'
-end;
+    begin
+        exec sys.sp_cdc_enable_db 'nds_odse'
+    end;
 select name, is_cdc_enabled
 FROM sys.databases
-WHERE name = 'nbs_odse' if not exists (SELECT name, is_tracked_by_cdc
+WHERE name = 'nbs_odse'
+
+
+if not exists (SELECT name, is_tracked_by_cdc
                FROM sys.tables
                WHERE is_tracked_by_cdc = 1
                  and name = 'Act')
-begin
-exec sys.sp_cdc_enable_table @source_schema = N'dbo', @source_name = N'Act', @role_name = NULL;
-end;
+    begin
+        exec sys.sp_cdc_enable_table @source_schema = N'dbo', @source_name = N'Act', @role_name = NULL;
+    end;
 
 SELECT name, is_tracked_by_cdc
 FROM sys.tables
@@ -206,25 +166,17 @@ end;
 
 */
 
-USE
-[master];
-IF
-NOT EXISTS (SELECT name
+USE [master];
+IF NOT EXISTS (SELECT name
                FROM sys.server_principals
                WHERE name = 'test_login2')
-    CREATE
-LOGIN [test_login2] WITH PASSWORD =N'test123', DEFAULT_DATABASE = [master], DEFAULT_LANGUAGE = [us_english], CHECK_EXPIRATION = OFF, CHECK_POLICY = OFF;
+    CREATE LOGIN [test_login2] WITH PASSWORD =N'test123', DEFAULT_DATABASE = [master], DEFAULT_LANGUAGE = [us_english], CHECK_EXPIRATION = OFF, CHECK_POLICY = OFF;
 
 
-USE
-[RDB];
-IF
-NOT EXISTS (SELECT name
+USE [RDB];
+IF NOT EXISTS (SELECT name
                FROM sys.database_principals
                WHERE name = 'test_user2')
-    CREATE
-USER [test_user2] FOR LOGIN [test_login2] WITH DEFAULT_SCHEMA =[dbo];
-ALTER
-ROLE [db_owner] ADD MEMBER [test_user2];
-ALTER
-LOGIN test_login2 with password = 'test1234';
+    CREATE USER [test_user2] FOR LOGIN [test_login2] WITH DEFAULT_SCHEMA =[dbo];
+ALTER ROLE [db_owner] ADD MEMBER [test_user2];
+ALTER LOGIN test_login2 with password = 'test1234';
