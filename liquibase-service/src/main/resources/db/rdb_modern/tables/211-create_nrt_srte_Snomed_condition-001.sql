021f8ac4e6c6859fe5af0e72fa2c27da2df8ef40
Upasana Pattnaik <129798209+upasanapattnaik-eq@users.noreply.github.com>
2025-06-18 15:15:31 -0400

    CNDE-2380: Fix for Kafka sink error on snomed_condition (#488)

--- liquibase-service/src/main/resources/db/rdb_modern/tables/211-create_nrt_srte_Snomed_condition-001.sql
+++ liquibase-service/src/main/resources/db/rdb_modern/tables/211-create_nrt_srte_Snomed_condition-001.sql
@@ -1,7 +1,4 @@
-IF NOT EXISTS (SELECT 1
-               FROM sysobjects
-               WHERE name = 'nrt_srte_Snomed_condition'
-                 and xtype = 'U')
+IF NOT EXISTS (SELECT 1 FROM sysobjects WHERE name = 'nrt_srte_Snomed_condition' and xtype = 'U')
     BEGIN
         CREATE TABLE dbo.nrt_srte_Snomed_condition
         (
@@ -14,4 +11,10 @@ IF NOT EXISTS (SELECT 1
             effective_from_time datetime     NULL,
             effective_to_time   datetime     NULL
         ) ON [PRIMARY]
+    END;
+
+IF NOT EXISTS(SELECT 1 FROM sys.objects WHERE type = 'PK' AND object_id = OBJECT_ID('nrt_srte_Snomed_condition'))
+    BEGIN
+        ALTER TABLE dbo.nrt_srte_Snomed_condition
+        ADD CONSTRAINT pk_nrt_srte_Snomed_condition PRIMARY KEY (snomed_cd);
     END;
