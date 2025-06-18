ef4e9614cf60b82ddf3d15e3accad12ac799593b
upasanapattnaik-eq <upattnaik@enquizit.com>
2025-06-18 16:55:21 -0400

    CNDE-2380: update primary key condition

--- liquibase-service/src/main/resources/db/rdb_modern/tables/211-create_nrt_srte_Snomed_condition-001.sql
+++ liquibase-service/src/main/resources/db/rdb_modern/tables/211-create_nrt_srte_Snomed_condition-001.sql
@@ -13,8 +13,8 @@ IF NOT EXISTS (SELECT 1 FROM sysobjects WHERE name = 'nrt_srte_Snomed_condition'
         ) ON [PRIMARY]
     END;
 
-IF NOT EXISTS(SELECT 1 FROM sys.objects WHERE type = 'PK' AND object_id = OBJECT_ID('nrt_srte_Snomed_condition'))
-    BEGIN
-        ALTER TABLE dbo.nrt_srte_Snomed_condition
-        ADD CONSTRAINT pk_nrt_srte_Snomed_condition PRIMARY KEY (snomed_cd);
-    END;
+IF NOT EXISTS(SELECT 1 FROM sys.objects WHERE type = 'PK'  AND parent_object_id = OBJECT_ID('dbo.nrt_srte_Snomed_condition'))
+BEGIN
+    ALTER TABLE dbo.nrt_srte_Snomed_condition
+    ADD CONSTRAINT pk_nrt_srte_Snomed_condition PRIMARY KEY (snomed_cd);
+END;
