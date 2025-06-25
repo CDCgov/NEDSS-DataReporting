6db7a73624761812e77eeeb0e1a58f9f5fbc84af
Joshua Mills <joshua.mills@cdw.com>
2025-06-25 09:26:42 -0400

    CNDE-2707: sproc updates and config nrt table

---
+++ liquibase-service/src/main/resources/db/rdb_modern/routines/345-sp_event_metric_cleanup_postprocessing-001.sql
@@ -0,0 +1,124 @@
+IF EXISTS (SELECT * FROM sysobjects WHERE  id = object_id(N'[dbo].[sp_event_metric_cleanup_postprocessing]') 
+	AND OBJECTPROPERTY(id, N'IsProcedure') = 1
+)
+BEGIN
+    DROP PROCEDURE [dbo].[sp_event_metric_cleanup_postprocessing]
+END
+GO 
+
+CREATE PROCEDURE [dbo].[sp_event_metric_cleanup_postprocessing]
+    @debug bit = 'false'
+AS
+BEGIN
+
+    DECLARE @batch_id BIGINT;
+    SET @batch_id = cast((format(getdate(),'yyMMddHHmmssffff')) AS BIGINT);
+    PRINT @batch_id;
+    DECLARE @RowCount_no INT;
+    DECLARE @Proc_Step_no FLOAT= 0;
+    DECLARE @Proc_Step_Name VARCHAR(200)= '';
+	DECLARE @Dataflow_Name VARCHAR(200) = 'Event Metric Cleanup POST-Processing';
+	DECLARE @Package_Name VARCHAR(200) = 'sp_event_metric_cleanup_postprocessing';
+
+    BEGIN TRY
+        
+
+        SET @Proc_Step_Name = 'SP_Start';
+
+        INSERT INTO dbo.job_flow_log ( batch_id
+                                    , [Dataflow_Name]
+                                    , [package_Name]
+                                    , [Status_Type]
+                                    , [step_number]
+                                    , [step_name]
+                                    , [row_count])
+        VALUES ( @batch_id
+            , @Dataflow_Name
+            , @Package_Name
+            , 'START'
+            , @Proc_Step_no
+            , @Proc_Step_Name
+            , 0);
+        
+--------------------------------------------------------------------------------------------------------
+
+
+        SET
+            @PROC_STEP_NO = @PROC_STEP_NO + 1;
+        SET
+            @PROC_STEP_NAME = 'GET CONFIG VALUE';
+
+        
+        DECLARE @metrics_gobackby_days INTEGER;
+
+        SET @metrics_gobackby_days = CAST((SELECT MAX(config_value) FROM NBS_ODSE.dbo.NBS_configuration WITH (NOLOCK)
+                                        WHERE config_key = 'METRICS_GOBACKBY_DAYS') AS INTEGER);
+
+        if @debug = 'true'
+            SELECT @Proc_Step_Name, @metrics_gobackby_days;
+
+        INSERT INTO [dbo].[job_flow_log] 
+		(batch_id, [Dataflow_Name], [package_Name], [Status_Type], [step_number], [step_name], [row_count])
+        VALUES (@batch_id, @Dataflow_Name, @Package_Name, 'START', @Proc_Step_no, @Proc_Step_name, @RowCount_no);
+
+
+--------------------------------------------------------------------------------------------------------
+
+
+        SET
+            @PROC_STEP_NO = @PROC_STEP_NO + 1;
+        SET
+            @PROC_STEP_NAME = 'DELETE OLD EVENTS FROM dbo.EVENT_METRIC';
+
+        
+        DELETE em 
+        FROM dbo.EVENT_METRIC em
+        WHERE DATEDIFF(day, ADD_TIME, GETDATE()) > 730;
+        
+        SELECT @ROWCOUNT_NO = @@ROWCOUNT; 
+
+        IF @debug = 'true'
+            SELECT @Proc_Step_Name, @RowCount_no AS deleted_rows;
+
+        INSERT INTO [dbo].[job_flow_log] 
+		(batch_id, [Dataflow_Name], [package_Name], [Status_Type], [step_number], [step_name], [row_count])
+        VALUES (@batch_id, @Dataflow_Name, @Package_Name, 'START', @Proc_Step_no, @Proc_Step_name, @RowCount_no);
+
+
+--------------------------------------------------------------------------------------------------------
+
+
+        SET @Proc_Step_no = 999;
+        SET @Proc_Step_Name = 'SP_COMPLETE';
+        SELECT @ROWCOUNT_NO = 0;
+
+        INSERT INTO [dbo].[job_flow_log] 
+		(batch_id, [Dataflow_Name], [package_Name], [Status_Type], [step_number], [step_name], [row_count])
+        VALUES (@batch_id, @Dataflow_Name, @Package_Name, 'COMPLETE', 999, @Proc_Step_name, @RowCount_no);
+    
+-------------------------------------------------------------------------------------------
+    END TRY
+
+    BEGIN CATCH
+
+        IF @@TRANCOUNT > 0   ROLLBACK TRANSACTION;
+
+        -- Construct the error message string with all details:
+            DECLARE @FullErrorMessage VARCHAR(8000) =
+                'Error Number: ' + CAST(ERROR_NUMBER() AS VARCHAR(10)) + CHAR(13) + CHAR(10) +  -- Carriage return and line feed for new lines
+                'Error Severity: ' + CAST(ERROR_SEVERITY() AS VARCHAR(10)) + CHAR(13) + CHAR(10) +
+                'Error State: ' + CAST(ERROR_STATE() AS VARCHAR(10)) + CHAR(13) + CHAR(10) +
+                'Error Line: ' + CAST(ERROR_LINE() AS VARCHAR(10)) + CHAR(13) + CHAR(10) +
+                'Error Message: ' + ERROR_MESSAGE();
+
+
+            INSERT INTO [dbo].[job_flow_log] 
+            (batch_id, [Dataflow_Name], [package_Name], [Status_Type], [step_number], [step_name], [Error_Description], [row_count])
+            VALUES (@batch_id, @Dataflow_Name, @Package_Name, 'ERROR', @Proc_Step_no, @Proc_Step_name, @FullErrorMessage, 0);
+
+        return -1 ;
+
+    END CATCH
+
+END;
+
