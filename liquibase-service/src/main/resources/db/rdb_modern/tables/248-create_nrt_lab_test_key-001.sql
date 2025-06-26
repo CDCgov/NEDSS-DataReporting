7984e38893dcf2c70f08b917ff44112ea9d2ca6d
Upasana Pattnaik <129798209+upasanapattnaik-eq@users.noreply.github.com>
2025-06-26 14:01:58 -0400

    Hotfix: Remove primary key constraint and key=1 for nrt_lab_test_key (#499)

--- liquibase-service/src/main/resources/db/rdb_modern/tables/248-create_nrt_lab_test_key-001.sql
+++ liquibase-service/src/main/resources/db/rdb_modern/tables/248-create_nrt_lab_test_key-001.sql
@@ -8,23 +8,12 @@ IF NOT EXISTS (SELECT 1 FROM sysobjects WHERE name = 'nrt_lab_test_key' and xtyp
 		updated_dttm DATETIME2 DEFAULT getdate()           
 		);
 
-		-- Insert Key = 1 with LAB_TEST_UID = NULL
-		INSERT INTO [dbo].nrt_lab_test_key(LAB_TEST_UID)
-		VALUES (NULL)
-
 		--RESEED [dbo].nrt_lab_test_key table 
 		DECLARE @max BIGINT;
 		SELECT @max=max(LAB_TEST_KEY) from [dbo].LAB_TEST;
 		SELECT @max;
 		IF @max IS NULL   --check when max is returned as null
-			SET @max = 2; -- default to 2, default record with key = 1 is already created
+			SET @max = 1; -- default to 1, default record with key = 1 is already created
 		DBCC CHECKIDENT ('[dbo].nrt_lab_test_key', RESEED, @max);
 
 	END;
-
-IF NOT EXISTS(SELECT 1 FROM sys.objects WHERE type = 'PK' AND parent_object_id = OBJECT_ID('dbo.nrt_lab_test_key'))
-    BEGIN
-        ALTER TABLE dbo.nrt_lab_test_key
-        ADD CONSTRAINT pk_nrt_lab_test_key PRIMARY KEY (LAB_TEST_KEY);
-    END
-
