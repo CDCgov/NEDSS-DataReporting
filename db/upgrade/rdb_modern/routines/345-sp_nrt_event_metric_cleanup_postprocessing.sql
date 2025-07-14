d3af73b505ba0fbe064b2b9d8959f610fa59f76e
Joshua Mills <joshua.mills@cdw.com>
2025-07-14 11:27:26 -0400

    CNDE-2969: record adding functionality for event metric cleanup job

--- db/upgrade/rdb_modern/routines/345-sp_nrt_event_metric_cleanup_postprocessing.sql
+++ db/upgrade/rdb_modern/routines/345-sp_nrt_event_metric_cleanup_postprocessing.sql
@@ -68,22 +68,195 @@ BEGIN
         SET
             @PROC_STEP_NO = @PROC_STEP_NO + 1;
         SET
-            @PROC_STEP_NAME = 'DELETE OLD EVENTS FROM dbo.EVENT_METRIC';
+            @PROC_STEP_NAME = 'GET COUNT IN dbo.EVENT_METRIC_INC';
 
         
-        DELETE em 
-        FROM dbo.EVENT_METRIC em
-        WHERE DATEDIFF(day, ADD_TIME, GETDATE()) > @metrics_gobackby_days;
+        DECLARE @em_inc_current_records INTEGER = (
+            SELECT COUNT(*) FROM dbo.EVENT_METRIC_INC WITH (NOLOCK)
+                WHERE DATEDIFF(day, ADD_TIME, GETDATE()) <= @metrics_gobackby_days
+        );
         
         SELECT @ROWCOUNT_NO = @@ROWCOUNT; 
 
         IF @debug = 'true'
-            SELECT @Proc_Step_Name, @RowCount_no AS deleted_rows;
+            SELECT @Proc_Step_Name, @em_inc_current_records AS em_inc_current_records;
 
         INSERT INTO [dbo].[job_flow_log] 
 		(batch_id, [Dataflow_Name], [package_Name], [Status_Type], [step_number], [step_name], [row_count])
         VALUES (@batch_id, @Dataflow_Name, @Package_Name, 'START', @Proc_Step_no, @Proc_Step_name, @RowCount_no);
 
+
+--------------------------------------------------------------------------------------------------------
+
+
+        SET
+            @PROC_STEP_NO = @PROC_STEP_NO + 1;
+        SET
+            @PROC_STEP_NAME = 'GET COUNT IN dbo.EVENT_METRIC';
+
+        
+        DECLARE @em_current_records INTEGER = (
+            SELECT COUNT(*) FROM dbo.EVENT_METRIC WITH (NOLOCK)
+        );
+        
+        SELECT @ROWCOUNT_NO = @@ROWCOUNT; 
+
+        IF @debug = 'true'
+            SELECT @Proc_Step_Name, @em_current_records AS em_current_records;
+
+        INSERT INTO [dbo].[job_flow_log] 
+		(batch_id, [Dataflow_Name], [package_Name], [Status_Type], [step_number], [step_name], [row_count])
+        VALUES (@batch_id, @Dataflow_Name, @Package_Name, 'START', @Proc_Step_no, @Proc_Step_name, @RowCount_no);
+
+
+--------------------------------------------------------------------------------------------------------
+
+
+        SET
+            @PROC_STEP_NO = @PROC_STEP_NO + 1;
+        SET
+            @PROC_STEP_NAME = 'CREATE LOAD FLAG';
+
+        /*
+            If the number of records that would be valid in EVENT_METRIC is greater in
+            EVENT_METRIC_INC.
+
+            When records need to be loaded, we will SKIP the delete step. The states
+            where records need to be loaded and records need to be deleted should be
+            mutually exclusive.
+        */
+        DECLARE @load_records bit;
+        SET @load_records = CASE 
+                                WHEN @em_inc_current_records > @em_current_records THEN 1
+                                ELSE 0
+                            END;
+        
+        SELECT @ROWCOUNT_NO = @@ROWCOUNT; 
+
+        IF @debug = 'true'
+            SELECT @Proc_Step_Name, @load_records AS load_flag;
+
+        INSERT INTO [dbo].[job_flow_log] 
+		(batch_id, [Dataflow_Name], [package_Name], [Status_Type], [step_number], [step_name], [row_count])
+        VALUES (@batch_id, @Dataflow_Name, @Package_Name, 'START', @Proc_Step_no, @Proc_Step_name, @RowCount_no);
+
+
+--------------------------------------------------------------------------------------------------------
+
+        IF (@load_records = 1)
+        BEGIN
+
+        BEGIN TRANSACTION
+            SET
+                @PROC_STEP_NO = @PROC_STEP_NO + 1;
+            SET
+                @PROC_STEP_NAME = 'ADD RECORDS TO dbo.EVENT_METRIC';
+
+            INSERT INTO dbo.EVENT_METRIC
+            (
+                [EVENT_TYPE],
+                [EVENT_UID], 
+                [LOCAL_ID], 
+                [LOCAL_PATIENT_ID], 
+                [CONDITION_CD], 
+                [CONDITION_DESC_TXT],
+                [PROG_AREA_CD], 
+                [PROG_AREA_DESC_TXT], 
+                [PROGRAM_JURISDICTION_OID], 
+                [JURISDICTION_CD],
+                [JURISDICTION_DESC_TXT],
+                [RECORD_STATUS_CD], 
+                [RECORD_STATUS_DESC_TXT], 
+                [RECORD_STATUS_TIME], 
+                [ELECTRONIC_IND], 
+                [STATUS_CD],
+                [STATUS_DESC_TXT], 
+                [STATUS_TIME], 
+                [ADD_TIME], 
+                [ADD_USER_ID], 
+                [LAST_CHG_TIME], 
+                [LAST_CHG_USER_ID],
+                [CASE_CLASS_CD], 
+                [CASE_CLASS_DESC_TXT], 
+                [INVESTIGATION_STATUS_CD], 
+                [INVESTIGATION_STATUS_DESC_TXT],
+                [ADD_USER_NAME], 
+                [LAST_CHG_USER_NAME]
+            )
+            SELECT
+                emi.[EVENT_TYPE],
+                emi.[EVENT_UID], 
+                emi.[LOCAL_ID], 
+                emi.[LOCAL_PATIENT_ID], 
+                emi.[CONDITION_CD], 
+                emi.[CONDITION_DESC_TXT],
+                emi.[PROG_AREA_CD], 
+                emi.[PROG_AREA_DESC_TXT], 
+                emi.[PROGRAM_JURISDICTION_OID], 
+                emi.[JURISDICTION_CD],
+                emi.[JURISDICTION_DESC_TXT],
+                emi.[RECORD_STATUS_CD], 
+                emi.[RECORD_STATUS_DESC_TXT], 
+                emi.[RECORD_STATUS_TIME], 
+                emi.[ELECTRONIC_IND], 
+                emi.[STATUS_CD],
+                emi.[STATUS_DESC_TXT], 
+                emi.[STATUS_TIME], 
+                emi.[ADD_TIME], 
+                emi.[ADD_USER_ID], 
+                emi.[LAST_CHG_TIME], 
+                emi.[LAST_CHG_USER_ID],
+                emi.[CASE_CLASS_CD], 
+                emi.[CASE_CLASS_DESC_TXT], 
+                emi.[INVESTIGATION_STATUS_CD], 
+                emi.[INVESTIGATION_STATUS_DESC_TXT],
+                emi.[ADD_USER_NAME], 
+                emi.[LAST_CHG_USER_NAME]
+            FROM (
+                SELECT * FROM dbo.EVENT_METRIC_INC WITH (NOLOCK)
+                WHERE DATEDIFF(day, ADD_TIME, GETDATE()) <= @metrics_gobackby_days
+                    AND ADD_TIME <= (SELECT MIN(ADD_TIME) FROM dbo.EVENT_METRIC WITH (NOLOCK))
+            ) emi
+            LEFT JOIN dbo.EVENT_METRIC em WITH (NOLOCK)
+                ON emi.EVENT_TYPE = em.EVENT_TYPE
+                    AND emi.EVENT_UID = em.EVENT_UID
+            WHERE em.EVENT_TYPE IS NULL AND em.EVENT_UID IS NULL;
+
+
+            SELECT @ROWCOUNT_NO = @@ROWCOUNT; 
+
+            IF @debug = 'true'
+                SELECT @Proc_Step_Name, @RowCount_no AS added_rows;
+
+            INSERT INTO [dbo].[job_flow_log] 
+            (batch_id, [Dataflow_Name], [package_Name], [Status_Type], [step_number], [step_name], [row_count])
+            VALUES (@batch_id, @Dataflow_Name, @Package_Name, 'START', @Proc_Step_no, @Proc_Step_name, @RowCount_no);
+        COMMIT TRANSACTION;
+
+        END
+        ELSE 
+
+        BEGIN
+            SET
+                @PROC_STEP_NO = @PROC_STEP_NO + 1;
+            SET
+                @PROC_STEP_NAME = 'DELETE OLD EVENTS FROM dbo.EVENT_METRIC';
+
+            
+            DELETE em 
+            FROM dbo.EVENT_METRIC em
+            WHERE DATEDIFF(day, ADD_TIME, GETDATE()) > @metrics_gobackby_days;
+            
+            SELECT @ROWCOUNT_NO = @@ROWCOUNT; 
+
+            IF @debug = 'true'
+                SELECT @Proc_Step_Name, @RowCount_no AS deleted_rows;
+
+            INSERT INTO [dbo].[job_flow_log] 
+            (batch_id, [Dataflow_Name], [package_Name], [Status_Type], [step_number], [step_name], [row_count])
+            VALUES (@batch_id, @Dataflow_Name, @Package_Name, 'START', @Proc_Step_no, @Proc_Step_name, @RowCount_no);
+
+        END;
 
 --------------------------------------------------------------------------------------------------------
 
