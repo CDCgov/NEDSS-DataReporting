0b0f30f8de35421b7f8d1587c0832b055b5358eb
Duc Nguyen <ukk2@cdc.gov>
2025-08-25 10:07:20 -0700

    Change Exception field to Max

--- liquibase-service/src/main/resources/db/005-rdb_modern/tables/257-create_rtr_dead_letter_log-001.sql
+++ liquibase-service/src/main/resources/db/005-rdb_modern/tables/257-create_rtr_dead_letter_log-001.sql
@@ -7,7 +7,7 @@ CREATE TABLE rtr_dead_letter_log (
                                      original_consumer_group NVARCHAR(255) NOT NULL,
                                      exception_stack_trace NVARCHAR(MAX) NULL,
                                      exception_message NVARCHAR(MAX) NULL,
-                                     exception_fqcn NVARCHAR(255) NULL,
-                                     exception_cause_fqcn NVARCHAR(255) NULL,
+                                     exception_fqcn NVARCHAR(MAX) NULL,
+                                     exception_cause_fqcn NVARCHAR(MAX) NULL,
                                      received_at  datetime2 NOT NULL,
 );
