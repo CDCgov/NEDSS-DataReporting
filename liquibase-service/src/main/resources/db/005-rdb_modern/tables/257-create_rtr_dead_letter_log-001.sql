38c54e9fe66b6d01eeb95c5e8b18ef6a4b5b81a6
Duc Nguyen <ukk2@cdc.gov>
2025-08-25 10:10:59 -0700

    format sql scrfipt

--- liquibase-service/src/main/resources/db/005-rdb_modern/tables/257-create_rtr_dead_letter_log-001.sql
+++ liquibase-service/src/main/resources/db/005-rdb_modern/tables/257-create_rtr_dead_letter_log-001.sql
@@ -1,13 +1,17 @@
-IF NOT EXISTS (SELECT 1 FROM sysobjects WHERE name = 'rtr_dead_letter_log' and xtype = 'U')
-CREATE TABLE rtr_dead_letter_log (
-                                     id bigint IDENTITY(1,1) PRIMARY KEY NOT NULL,
-                                     origin_topic NVARCHAR(255) NOT NULL,
-                                     payload_key NVARCHAR(MAX) NOT NULL,
-                                     payload NVARCHAR(MAX) NOT NULL,
-                                     original_consumer_group NVARCHAR(255) NOT NULL,
-                                     exception_stack_trace NVARCHAR(MAX) NULL,
-                                     exception_message NVARCHAR(MAX) NULL,
-                                     exception_fqcn NVARCHAR(MAX) NULL,
-                                     exception_cause_fqcn NVARCHAR(MAX) NULL,
-                                     received_at  datetime2 NOT NULL,
+IF NOT EXISTS (SELECT 1
+               FROM sysobjects
+               WHERE name = 'rtr_dead_letter_log'
+                 and xtype = 'U')
+CREATE TABLE rtr_dead_letter_log
+(
+    id                      bigint IDENTITY (1,1) PRIMARY KEY NOT NULL,
+    origin_topic            NVARCHAR(255)                     NOT NULL,
+    payload_key             NVARCHAR(MAX)                     NOT NULL,
+    payload                 NVARCHAR(MAX)                     NOT NULL,
+    original_consumer_group NVARCHAR(255)                     NOT NULL,
+    exception_stack_trace   NVARCHAR(MAX)                     NULL,
+    exception_message       NVARCHAR(MAX)                     NULL,
+    exception_fqcn          NVARCHAR(MAX)                     NULL,
+    exception_cause_fqcn    NVARCHAR(MAX)                     NULL,
+    received_at             datetime2                         NOT NULL,
 );
