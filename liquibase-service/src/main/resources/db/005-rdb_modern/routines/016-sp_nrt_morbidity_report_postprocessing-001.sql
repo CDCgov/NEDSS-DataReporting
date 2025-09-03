4f43117a722ed32c3eb1eb9c3f346db84819e655
Jayasudha Sundaram <jayasudha.sundaram@cdwg.com>
2025-09-03 15:34:13 -0400

    CNDE-3046 : added the missing delete logic in morbidity report postprocessing sp (#581)

--- liquibase-service/src/main/resources/db/005-rdb_modern/routines/016-sp_nrt_morbidity_report_postprocessing-001.sql
+++ liquibase-service/src/main/resources/db/005-rdb_modern/routines/016-sp_nrt_morbidity_report_postprocessing-001.sql
@@ -325,6 +325,46 @@ BEGIN
 
         if @debug = 'true' SELECT 'DEBUG: tmp_morb_root_keyvalue', * FROM #tmp_morb_root;
 
+        -- ADD MISSING DELETE LOGIC
+        BEGIN TRANSACTION;
+
+        SET @PROC_STEP_NO = @PROC_STEP_NO + 1;
+        SET @PROC_STEP_NAME = 'Disassociate LAB_TEST_RESULT only for morb reports without investigations';
+
+-- Only move lab results to default bucket when morbidity report loses investigation association
+        UPDATE dbo.LAB_TEST_RESULT
+        SET morb_rpt_key = 1,
+            RDB_LAST_REFRESH_TIME = GETDATE()
+          WHERE morb_rpt_key IN (
+            SELECT tmr.morb_rpt_key
+            FROM #tmp_morb_root tmr
+            WHERE tmr.morb_rpt_key IS NOT NULL
+              AND (
+                -- No investigation associated
+                tmr.associated_phc_uids IS NULL
+                    OR LTRIM(RTRIM(tmr.associated_phc_uids)) = ''
+                    OR tmr.record_status_cd = 'INACTIVE'
+                    -- Or investigation no longer exists (handle comma-separated values and data type conversion)
+                    OR NOT EXISTS (
+                    SELECT 1 FROM dbo.Investigation inv
+                    WHERE inv.case_uid IN (
+                        SELECT TRY_CAST(LTRIM(RTRIM(value)) AS BIGINT)
+                        FROM STRING_SPLIT(tmr.associated_phc_uids, ',')
+                        WHERE TRY_CAST(LTRIM(RTRIM(value)) AS BIGINT) IS NOT NULL
+                    )
+                )
+                )
+        )
+          AND morb_rpt_key <> 1;
+
+        SELECT @RowCount_no = @@ROWCOUNT;
+
+        INSERT INTO [dbo].[job_flow_log]
+        (batch_id,[Dataflow_Name],[package_Name] ,[Status_Type],[step_number],[step_name],[row_count])
+        VALUES  (@BATCH_ID,@Dataflow_Name,@Package_Name,'START',@PROC_STEP_NO,@PROC_STEP_NAME,@ROWCOUNT_NO);
+
+
+        COMMIT TRANSACTION;
 
         SELECT @RowCount_no = @@ROWCOUNT;
 
