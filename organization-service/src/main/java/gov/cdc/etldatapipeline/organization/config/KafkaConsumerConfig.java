2fa33b247a45dcda4c845504f2341b906e553343
Anand Logan <117679402+alog-enquizit@users.noreply.github.com>
2025-09-17 12:21:12 -0700

    Rtr perf sep172025 : fix a kafka consumer config bug (#608)

--- organization-service/src/main/java/gov/cdc/etldatapipeline/organization/config/KafkaConsumerConfig.java
+++ organization-service/src/main/java/gov/cdc/etldatapipeline/organization/config/KafkaConsumerConfig.java
@@ -32,9 +32,6 @@ public class KafkaConsumerConfig {
     @Value("${spring.kafka.consumer.maxPollRecs}")
     private String maxPollRecords;
 
-    @Value("${spring.kafka.consumer.maxPollRecs}")
-    private String maxPollRecords = "";
-
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         final Map<String, Object> config = new HashMap<>();
