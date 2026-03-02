5e1f7c633841f190b9c4a8be0fa4d4afb052840b
hawley-skylight <patrick.hawley@skylight.digital>
2026-03-02 12:53:56 -0500

    APP-171: add algorithms for workflow decision support (#626)

---
+++ performance-testing/workflow_decision/Investigate_HIV_positive_test.xml.sql
@@ -0,0 +1,99 @@
+USE NBS_ODSE;
+GO
+
+INSERT INTO DSM_algorithm (
+    algorithm_nm,
+    event_type,
+    condition_list,
+    resulted_test_list,
+    frequency,
+    apply_to,
+    sending_system_list,
+    reporting_system_list,
+    event_action,
+    algorithm_payload,
+    admin_comment,
+    status_cd,
+    status_time,
+    last_chg_user_id,
+    last_chg_time
+) VALUES (
+    'Investigate_HIV_positive_test',                              -- algorithm_nm
+    '11648804',                                -- event_type
+    '900',                            -- condition_list
+    '10351-5',                             -- resulted_test_list
+    '1',                                 -- frequency
+    '1',                                  -- apply_to
+    NULL,                                           -- sending_system_list
+    NULL,                                           -- reporting_system_list
+    '1',                                            -- event_action
+    '<Algorithm xmlns="http://www.cdc.gov/NEDSS">
+  <AlgorithmName>Investigate_HIV_positive_test</AlgorithmName>
+  <Event>
+    <Code>11648804</Code>
+    <CodeDescTxt>Laboratory Report</CodeDescTxt>
+    <CodeSystemCode>2.16.840.1.113883.6.96</CodeSystemCode>
+  </Event>
+  <Frequency>
+    <Code>1</Code>
+    <CodeDescTxt>Real-Time</CodeDescTxt>
+    <CodeSystemCode>L</CodeSystemCode>
+  </Frequency>
+  <AppliesToEntryMethods>
+    <EntryMethod>
+      <Code>1</Code>
+      <CodeDescTxt>Electronic Document</CodeDescTxt>
+      <CodeSystemCode>L</CodeSystemCode>
+    </EntryMethod>
+  </AppliesToEntryMethods>
+  <InvestigationType>PG_HIV_Investigation</InvestigationType>
+  <ApplyToConditions>
+    <Condition>
+      <Code>900</Code>
+      <CodeDescTxt>HIV</CodeDescTxt>
+      <CodeSystemCode>L</CodeSystemCode>
+    </Condition>
+  </ApplyToConditions>
+  <Comment/>
+  <ElrAdvancedCriteria>
+    <EventDateLogic>
+      <ElrTimeLogic>
+        <ElrTimeLogicInd>
+          <Code>N</Code>
+        </ElrTimeLogicInd>
+      </ElrTimeLogic>
+    </EventDateLogic>
+    <AndOrLogic>OR</AndOrLogic>
+    <ElrCriteria>
+      <ResultedTest>
+        <Code>10351-5</Code>
+        <CodeDescTxt>HIV 1 RNA (10351-5)</CodeDescTxt>
+      </ResultedTest>
+      <ElrCodedResultValue>
+        <Code>260373001</Code>
+        <CodeDescTxt>Detected (260373001)</CodeDescTxt>
+      </ElrCodedResultValue>
+    </ElrCriteria>
+    <InvLogic>
+      <InvLogicInd>
+        <Code>N</Code>
+      </InvLogicInd>
+    </InvLogic>
+  </ElrAdvancedCriteria>
+  <Action>
+    <CreateInvestigation>
+      <OnFailureToCreateInvestigation>
+        <Code>2</Code>
+        <CodeDescTxt>Retain Event Record</CodeDescTxt>
+        <CodeSystemCode>L</CodeSystemCode>
+      </OnFailureToCreateInvestigation>
+    </CreateInvestigation>
+  </Action>
+</Algorithm>',
+    '',                                             -- admin_comment
+    'A',                                            -- status_cd
+    GETDATE(),                                      -- status_time
+    -1,                                             -- last_chg_user_id
+    GETDATE()                                       -- last_chg_time
+);
+GO
