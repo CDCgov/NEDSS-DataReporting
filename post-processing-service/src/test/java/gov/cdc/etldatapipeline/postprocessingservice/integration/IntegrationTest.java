f5a08856fe1a1e48d797c289ae16df0e7827be2d
Michael Peels <m.peels.dev@gmail.com>
2026-03-09 17:10:59 -0400

    Add separate test datasource for NBS_ODSE user. Successful patient creation

--- post-processing-service/src/test/java/gov/cdc/etldatapipeline/postprocessingservice/integration/IntegrationTest.java
+++ post-processing-service/src/test/java/gov/cdc/etldatapipeline/postprocessingservice/integration/IntegrationTest.java
@@ -1,21 +1,34 @@
 package gov.cdc.etldatapipeline.postprocessingservice.integration;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.io.File;
 import java.time.Duration;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.ComposeContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.DockerImageName;
 
+import gov.cdc.etldatapipeline.postprocessingservice.integration.patient.PatientCreator;
+
+@SpringBootTest
+@ActiveProfiles("test")
 class IntegrationTest {
+
+    @Autowired
+    private PatientCreator patientCreator;
 
     @SuppressWarnings("resource")
     private static final ComposeContainer environment = new ComposeContainer(
             DockerImageName.parse("docker:25.0.5"),
             new File("../docker-compose.yaml"))
+            // List specific services to prevent launching wildfly container
             .withServices(
                     "nbs-mssql",
                     "liquibase",
@@ -23,16 +36,21 @@ class IntegrationTest {
                     "kafka",
                     "debezium",
                     "kafka-connect",
-                    "person-service",
-                    "post-processing-service")
+                    "person-service")
+            // Add liquibase specific log check and increase default timeout
             .waitingFor("liquibase",
                     Wait.forLogMessage("Migrations complete.*", 1).withStartupTimeout(Duration.ofMinutes(3)))
+            // Set a global startup timeout for ComposeContainer
             .withStartupTimeout(Duration.ofMinutes(10));
 
     @BeforeAll
     static void setUp() {
         // Start up necessary containers
         environment.start();
+
+        // TODO Initialize debezium connectors
+
+        // TODO Initialize kafka-sync connector
     }
 
     @AfterAll
@@ -42,13 +60,13 @@ class IntegrationTest {
     }
 
     @Test
-    void patientDataIsSuccessfullyProcessed() throws InterruptedException {
-        System.out.println("Starting test...");
-        Thread.sleep(Duration.ofSeconds(10)); // Testing that container comes up
-        System.out.println("Test complete...");
+    void patientDataIsSuccessfullyProcessed() {
         // Insert a patient into NBS_ODSE
+        long createdPatient = patientCreator.create();
+        assertThat(createdPatient).isNotZero();
 
         // Validate patient data arrives in D_PATIENT
+
     }
 
 }
---
+++ post-processing-service/src/test/java/gov/cdc/etldatapipeline/postprocessingservice/integration/patient/PatientCreator.java
@@ -0,0 +1,82 @@
+package gov.cdc.etldatapipeline.postprocessingservice.integration.patient;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Component;
+
+import gov.cdc.etldatapipeline.postprocessingservice.integration.id.IdGenerator;
+import gov.cdc.etldatapipeline.postprocessingservice.integration.id.IdGenerator.EntityType;
+import gov.cdc.etldatapipeline.postprocessingservice.integration.id.IdGenerator.GeneratedId;
+
+/**
+ * Responsible for creating and inserting patient data into the NBS_ODSE for
+ * integration testing
+ */
+@Component
+public class PatientCreator {
+
+    private IdGenerator idGenerator;
+    private JdbcClient client;
+
+    public PatientCreator(
+            final IdGenerator idGenerator,
+            @Qualifier("testClient") final JdbcClient client) {
+        this.idGenerator = idGenerator;
+        this.client = client;
+    }
+
+    private static final String CREATE_QUERY = """
+            insert into NBS_ODSE.dbo.Entity(entity_uid, class_cd) values (:id, 'PSN');
+
+            insert into NBS_ODSE.dbo.Person(
+                person_uid,
+                person_parent_uid,
+                local_id,
+                version_ctrl_nbr,
+                cd,
+                electronic_ind,
+                edx_ind,
+                add_time,
+                add_user_id,
+                last_chg_time,
+                last_chg_user_id,
+                record_status_cd,
+                record_status_time,
+                status_cd,
+                status_time
+            ) values (
+                :id,
+                :id,
+                :local,
+                1,
+                'PAT',
+                'N',
+                'Y',
+                :addedOn,
+                :addedBy,
+                :addedOn,
+                :addedBy,
+                'ACTIVE',
+                :addedOn,
+                'A',
+                :addedOn
+            );
+            """;
+
+    public long create() {
+
+        GeneratedId identifier = idGenerator.next(EntityType.PERSON);
+
+        this.client.sql(CREATE_QUERY)
+                .param("id", identifier.id())
+                .param("local", identifier.toLocalId())
+                .param("addedOn", LocalDateTime.now())
+                .param("addedBy", "9999")
+                .update();
+
+        return identifier.id();
+    }
+
+}
