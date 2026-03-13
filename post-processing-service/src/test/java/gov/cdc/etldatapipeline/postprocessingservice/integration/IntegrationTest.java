93a56c098598dc6229d2e2db9ed75571b15b429c
Michael Peels <m.peels.dev@gmail.com>
2026-03-13 11:16:33 -0400

    Create abstract base

--- post-processing-service/src/test/java/gov/cdc/etldatapipeline/postprocessingservice/integration/IntegrationTest.java
+++ post-processing-service/src/test/java/gov/cdc/etldatapipeline/postprocessingservice/integration/IntegrationTest.java
@@ -1,33 +1,19 @@
 package gov.cdc.etldatapipeline.postprocessingservice.integration;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import gov.cdc.etldatapipeline.postprocessingservice.integration.patient.PatientCreator;
-import gov.cdc.etldatapipeline.postprocessingservice.integration.rdb.DPatientFinder;
-import gov.cdc.etldatapipeline.postprocessingservice.integration.util.Await;
 import java.io.File;
 import java.time.Duration;
-import java.util.Optional;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.ComposeContainer;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest
 @ActiveProfiles("test")
-class IntegrationTest {
-
-  @Autowired private PatientCreator patientCreator;
-
-  @Autowired private DPatientFinder dPatientFinder;
+abstract class IntegrationTest {
 
   private static final Logger logger = LoggerFactory.getLogger(IntegrationTest.class);
   private static final Slf4jLogConsumer consumer = new Slf4jLogConsumer(logger);
@@ -68,19 +54,5 @@ class IntegrationTest {
   static void tearDown() {
     // Stop all containers
     environment.stop();
-  }
-
-  @Test
-  void patientDataIsSuccessfullyProcessed() {
-    // Insert a patient into NBS_ODSE
-    long createdPatient = patientCreator.create();
-    assertThat(createdPatient).isNotZero();
-
-    // Validate patient data arrives in D_PATIENT with retry
-    Optional<Long> dPatientKey =
-        Await.waitFor(dPatientFinder::findDPatientKeyWithRetry, createdPatient);
-
-    assertThat(dPatientKey).isPresent();
-    assertThat(dPatientKey.get()).isNotZero();
   }
 }
