f727873a8d4436086b6ce0adf9d2c0ea837dfcd9
Eric Buckley <eric.buckley@gmail.com>
2026-03-16 13:46:14 -0700

    adding a test for main

--- reporting-hydration-service/src/test/java/gov/cdc/etldatapipeline/reportinghydration/ReportingHydrationServiceApplicationTests.java
+++ reporting-hydration-service/src/test/java/gov/cdc/etldatapipeline/reportinghydration/ReportingHydrationServiceApplicationTests.java
@@ -4,7 +4,10 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.mock;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +18,23 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 class ReportingHydrationServiceApplicationTests {
 
   @Autowired private ApplicationContext context;
+
+  @Test
+  void testMain() {
+    try (MockedStatic<SpringApplication> mocked = Mockito.mockStatic(SpringApplication.class)) {
+      mocked
+          .when(
+              () ->
+                  SpringApplication.run(
+                      ReportingHydrationServiceApplication.class, new String[] {}))
+          .thenReturn(null);
+
+      ReportingHydrationServiceApplication.main(new String[] {});
+      mocked.verify(
+          () -> SpringApplication.run(ReportingHydrationServiceApplication.class, new String[] {}),
+          Mockito.times(1));
+    }
+  }
 
   @Test
   void contextLoads() {
