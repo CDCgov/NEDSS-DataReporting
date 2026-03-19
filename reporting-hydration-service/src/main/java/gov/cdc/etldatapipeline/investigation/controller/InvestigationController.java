794248fef23b6438600421aec19814763710d93e
Ada Ball <ada@adamball.org>
2026-03-19 10:21:55 -0500

    Remove unused import statements.

--- reporting-hydration-service/src/main/java/gov/cdc/etldatapipeline/investigation/controller/InvestigationController.java
+++ reporting-hydration-service/src/main/java/gov/cdc/etldatapipeline/investigation/controller/InvestigationController.java
@@ -4,8 +4,6 @@ import gov.cdc.etldatapipeline.investigation.service.KafkaProducerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
