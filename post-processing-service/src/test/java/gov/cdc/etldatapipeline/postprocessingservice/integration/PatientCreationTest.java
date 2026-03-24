b323e2366d952b00687353b13e1ec9307f5d2178
Ada Ball <ada@adamball.org>
2026-03-24 10:49:18 -0500

    Fix merge issue with import statement

--- post-processing-service/src/test/java/gov/cdc/etldatapipeline/postprocessingservice/integration/PatientCreationTest.java
+++ post-processing-service/src/test/java/gov/cdc/etldatapipeline/postprocessingservice/integration/PatientCreationTest.java
@@ -6,6 +6,7 @@ import gov.cdc.etldatapipeline.postprocessingservice.integration.patient.Patient
 import gov.cdc.etldatapipeline.postprocessingservice.integration.rdb.DPatientFinder;
 import gov.cdc.etldatapipeline.postprocessingservice.integration.util.Await;
 import java.util.Optional;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
