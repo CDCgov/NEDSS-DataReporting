b38d80358dc7d3a9c8f922464d1ea707414c94d6
Eric Buckley <eric.buckley@gmail.com>
2026-04-03 13:03:16 -0700

    refactor(db): split and parameterize user setup scripts
    Replace the monolithic 03-user-db_deploy_admin.sql with two
    specialized scripts: 07-rtr-liquibase-admin.sql and
    08-rtr-service-user.sql. This separation provides better
    modularity for deployment and runtime accounts.
    
    Use SQLCMD variables for usernames and passwords to allow injection
    from environment configuration. Ensure both local and RDS environments
    are handled correctly with appropriate server roles and database
    access levels.

---
+++ containers/db/initialize/08-rtr-service-user.sql
@@ -0,0 +1,76 @@
+-- ==========================================
+-- RTR Service User Setup
+-- $(RTR_SERVICE_USER_NAME) (Debezium, Kafka, Java Services)
+-- ==========================================
+
+USE [master];
+GO
+
+-- ------------------------------------------
+-- 1. Login Setup
+-- ------------------------------------------
+IF NOT EXISTS (SELECT name FROM sys.server_principals WHERE name = '$(RTR_SERVICE_USER_NAME)')
+BEGIN
+    CREATE LOGIN [$(RTR_SERVICE_USER_NAME)] WITH PASSWORD = N'$(RTR_SERVICE_USER_PASSWORD)', DEFAULT_DATABASE = [master], CHECK_EXPIRATION = OFF, CHECK_POLICY = OFF;
+    GRANT VIEW SERVER STATE TO [$(RTR_SERVICE_USER_NAME)];
+    PRINT 'Created login [$(RTR_SERVICE_USER_NAME)] and granted VIEW SERVER STATE';
+END
+
+-- ------------------------------------------
+-- 2. Database-Level Permissions: NBS_ODSE
+-- ------------------------------------------
+IF DB_ID('NBS_ODSE') IS NOT NULL
+BEGIN
+    EXEC('
+        USE [NBS_ODSE];
+        IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = ''$(RTR_SERVICE_USER_NAME)'')
+            CREATE USER [$(RTR_SERVICE_USER_NAME)] FOR LOGIN [$(RTR_SERVICE_USER_NAME)];
+        ALTER ROLE [db_datareader] ADD MEMBER [$(RTR_SERVICE_USER_NAME)];
+        GRANT INSERT, UPDATE, DELETE ON [dbo].[SubjectRaceInfo] TO [$(RTR_SERVICE_USER_NAME)];
+        GRANT INSERT, UPDATE, DELETE ON [dbo].[PublicHealthCaseFact] TO [$(RTR_SERVICE_USER_NAME)];
+    ');
+END
+
+-- ------------------------------------------
+-- 3. Database-Level Permissions: NBS_SRTE
+-- ------------------------------------------
+IF DB_ID('NBS_SRTE') IS NOT NULL
+BEGIN
+    EXEC('
+        USE [NBS_SRTE];
+        IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = ''$(RTR_SERVICE_USER_NAME)'')
+            CREATE USER [$(RTR_SERVICE_USER_NAME)] FOR LOGIN [$(RTR_SERVICE_USER_NAME)];
+        ALTER ROLE [db_datareader] ADD MEMBER [$(RTR_SERVICE_USER_NAME)];
+    ');
+END
+
+-- ------------------------------------------
+-- 4. Database-Level Permissions: RDB
+-- ------------------------------------------
+IF DB_ID('RDB') IS NOT NULL
+BEGIN
+    EXEC('
+        USE [RDB];
+        -- Service User (Needs db_owner ONLY if RDB is the target - non-UAT)
+        IF NOT EXISTS(SELECT 1 FROM NBS_ODSE.DBO.NBS_configuration WHERE config_key =''ENV'' AND config_value =''UAT'')
+        BEGIN
+            IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = ''$(RTR_SERVICE_USER_NAME)'')
+                CREATE USER [$(RTR_SERVICE_USER_NAME)] FOR LOGIN [$(RTR_SERVICE_USER_NAME)];
+            ALTER ROLE [db_owner] ADD MEMBER [$(RTR_SERVICE_USER_NAME)];
+        END
+    ');
+END
+
+-- ------------------------------------------
+-- 5. Database-Level Permissions: RDB_MODERN
+-- ------------------------------------------
+IF DB_ID('rdb_modern') IS NOT NULL
+BEGIN
+    EXEC('
+        USE [rdb_modern];
+        IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = ''$(RTR_SERVICE_USER_NAME)'')
+            CREATE USER [$(RTR_SERVICE_USER_NAME)] FOR LOGIN [$(RTR_SERVICE_USER_NAME)];
+        ALTER ROLE [db_owner] ADD MEMBER [$(RTR_SERVICE_USER_NAME)];
+    ');
+END
+GO
