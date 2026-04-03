0d7ccc12f69aa70384be18b3236865b1440b8700
Ada Ball <ada@adamball.org>
2026-04-03 11:43:38 -0500

    Rename ReportingPipelineServiceApplication and its test class for `reporting-pipeline-service`

--- reporting-pipeline-service/src/main/java/gov/cdc/etldatapipeline/ReportingPipelineServiceApplication.java
+++ reporting-pipeline-service/src/main/java/gov/cdc/etldatapipeline/ReportingPipelineServiceApplication.java
@@ -4,18 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Main entry point for the Reporting Hydration Service. This service is intended to consolidate
+ * Main entry point for the Reporting Pipeline Service. This service is intended to consolidate
  * various reporting microservices into a single application to simplify the ETL data pipeline.
  */
 @SpringBootApplication
-public class ReportingHydrationServiceApplication {
+public class ReportingPipelineServiceApplication {
 
   /**
-   * Starts the Reporting Hydration Service application.
+   * Starts the Reporting Pipeline Service application.
    *
    * @param args command-line arguments
    */
   public static void main(String[] args) {
-    SpringApplication.run(ReportingHydrationServiceApplication.class, args);
+    SpringApplication.run(ReportingPipelineServiceApplication.class, args);
   }
 }
