c123d44a1e326867fb1306992fa23e64a5d71c28
mehansen <merethe@skylight.digital>
2026-04-09 16:29:25 -0700

    fix reseeding new nrt_lab_test_key table

--- liquibase-service/src/main/resources/db/005-rdb_modern/tables/248-create_nrt_lab_test_key-001.sql
+++ liquibase-service/src/main/resources/db/005-rdb_modern/tables/248-create_nrt_lab_test_key-001.sql
@@ -8,12 +8,14 @@ IF NOT EXISTS (SELECT 1 FROM sysobjects WHERE name = 'nrt_lab_test_key' and xtyp
 		updated_dttm DATETIME2 DEFAULT getdate()           
 		);
 
-		--RESEED [dbo].nrt_lab_test_key table 
+		-- RESEED [dbo].nrt_lab_test_key table to align keys with LAB_TEST
 		DECLARE @max BIGINT;
 		SELECT @max=max(LAB_TEST_KEY) from [dbo].LAB_TEST;
 		SELECT @max;
-		IF @max IS NULL   --check when max is returned as null
-			SET @max = 1; -- default to 1, default record with key = 1 is already created
-		DBCC CHECKIDENT ('[dbo].nrt_lab_test_key', RESEED, @max);
+		IF @max IS NULL
+			SET @max = 1; -- LAB_TEST is expected to have a default null row with LAB_TEST_KEY = 1
+		-- when a table with no rows is reseeded, the third parameter will be the key value of the first added row, so
+        -- we must increment by 1 to prevent key collision with LAB_TEST
+		DBCC CHECKIDENT ('[dbo].nrt_lab_test_key', RESEED, @max+1);
 
 	END;
