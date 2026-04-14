6ac7afc4dea439b2a00ce59971524bf5835d505e
Michael Peels <109251240+mpeels@users.noreply.github.com>
2026-04-14 16:30:09 -0400

    APP-283 Morbidity Report test (#750)

--- reporting-pipeline-service/src/test/java/gov/cdc/nbs/report/pipeline/integration/support/Await.java
+++ reporting-pipeline-service/src/test/java/gov/cdc/nbs/report/pipeline/integration/support/Await.java
@@ -1,13 +1,25 @@
 package gov.cdc.nbs.report.pipeline.integration.support;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Optional;
-import java.util.function.Function;
+import java.util.function.Supplier;
+import org.json.JSONException;
+import org.skyscreamer.jsonassert.JSONCompare;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 
 public class Await {
 
   private static final Duration DEFAULT_RETRY_DELAY = Duration.ofSeconds(6);
   private static final int DEFAULT_MAX_RETRY = 20;
+  private static final ObjectMapper MAPPER =
+      new ObjectMapper()
+          .enable(SerializationFeature.INDENT_OUTPUT)
+          .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+          .setDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS"));
 
   private Await() {}
 
@@ -15,26 +27,23 @@ public class Await {
    * Calls the provided function until it returns a non-empty Optional or the retry limit is
    * reached.
    *
-   * @param <I> Generic Input
    * @param <O> Generic Output
-   * @param function Function that accepts a single parameter of type I and returns an
-   *     Optional{@literal <O>}
-   * @param parameter The single parameter of type I to be passed to the function
+   * @param function Supplier that returns an Optional{@literal <O>}s
    * @return
    */
-  public static <I, O> Optional<O> waitFor(Function<I, Optional<O>> function, I parameter) {
-    return waitFor(function, parameter, DEFAULT_MAX_RETRY, DEFAULT_RETRY_DELAY);
+  public static <O> Optional<O> waitFor(Supplier<Optional<O>> function) {
+    return waitFor(function, DEFAULT_MAX_RETRY, DEFAULT_RETRY_DELAY);
   }
 
   @SuppressWarnings("java:S2925") // this code sleeps while waiting.
-  public static <A, B> Optional<B> waitFor(
-      Function<A, Optional<B>> function, A parameter, int maxRetry, Duration retryDelay) {
+  public static <O> Optional<O> waitFor(
+      Supplier<Optional<O>> function, int maxRetry, Duration retryDelay) {
     int retryCount = 0;
-    Optional<B> result = Optional.empty();
+    Optional<O> result = Optional.empty();
 
     while (retryCount < maxRetry && result.isEmpty()) {
       retryCount += 1;
-      result = function.apply(parameter);
+      result = function.get();
 
       if (result.isEmpty()) {
         try {
@@ -42,6 +51,48 @@ public class Await {
         } catch (InterruptedException e) {
           return result;
         }
+      }
+    }
+    return result;
+  }
+
+  /**
+   * Calls the provided function until it returns a non-empty Optional that matches the expected
+   * value or the retry limit is reached. Compares results to the expected by using {@link
+   * JSONCompare#compareJSON} with compare mode set to LENIENT
+   *
+   * @param <O> Generic Output
+   * @param function Supplier that returns an Optional{@literal <O>}s
+   * @param expected The expected data that the query will return
+   * @return
+   */
+  public static <O> Optional<O> waitForMatch(Supplier<Optional<O>> function, String expected)
+      throws JsonProcessingException, JSONException {
+    return waitForMatch(function, expected, DEFAULT_MAX_RETRY, DEFAULT_RETRY_DELAY);
+  }
+
+  @SuppressWarnings("java:S2925") // this code sleeps while waiting.
+  public static <O> Optional<O> waitForMatch(
+      Supplier<Optional<O>> function, String expected, int maxRetry, Duration retryDelay)
+      throws JSONException, JsonProcessingException {
+    int retryCount = 0;
+    Optional<O> result = Optional.empty();
+
+    while (retryCount < maxRetry) {
+      retryCount += 1;
+      result = function.get();
+
+      if (result.isPresent()
+          && JSONCompare.compareJSON(
+                  expected, MAPPER.writeValueAsString(result.get()), JSONCompareMode.LENIENT)
+              .passed()) {
+        return result;
+      }
+
+      try {
+        Thread.sleep(retryDelay);
+      } catch (InterruptedException e) {
+        return result;
       }
     }
     return result;
--- reporting-pipeline-service/src/test/java/gov/cdc/nbs/report/pipeline/integration/support/data/patient/comment/PatientComment.java
+++
@@ -1,9 +0,0 @@
-package gov.cdc.nbs.report.pipeline.integration.support.data.patient.comment;
-
-import java.time.LocalDateTime;
-
-/**
- * Contains the data necessary for setting a comment on a patient profile. This comment is stored on
- * the Person table
- */
-public record PatientComment(LocalDateTime asOf, String comment) {}
