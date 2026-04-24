215dc10057f110afce5342c11bddd5305e251809
Michael Peels <m.peels.dev@gmail.com>
2026-04-23 20:00:24 -0400

    Add test

---
+++ reporting-pipeline-service/src/test/resources/testData/functional/fullPathSupervisorReview/040-approveNotification/setup.sql
@@ -0,0 +1,27 @@
+USE [NBS_ODSE];
+
+DECLARE @superuser_id BIGINT = 10009282;
+-- Adjust the UID declarations below manually so they remain unique across other tests.
+DECLARE @dbo_Act_act_uid_2 bigint = 20100409;
+DECLARE @dbo_Act_act_uid BIGINT = 20100408;
+
+-- STEP 4: ApproveNotification
+-- dbo.Notification
+-- step: 4
+UPDATE [dbo].[notification]
+SET    [last_chg_time] = N'2026-04-22T20:52:44.717',
+       [last_chg_user_id] = @superuser_id,
+       [record_status_cd] = N'APPROVED',
+       [record_status_time] = N'2026-04-22T20:52:44.717',
+       [txt] = N'Notification Approved 04/21/2026',
+       [version_ctrl_nbr] = Isnull([version_ctrl_nbr], 0) + 1
+WHERE  [notification_uid] = @dbo_Act_act_uid_2;
+
+-- dbo.PublicHealthCaseFact
+-- step: 4
+UPDATE [dbo].[publichealthcasefact]
+SET    [lastnotificationdate] = N'2026-04-22T20:52:44.717',
+       [lastnotificationsubmittedby] = @superuser_id,
+       [notifcurrentstate] = N'APPROVED',
+       [notitxt] = N'Notification Approved 04/21/2026'
+WHERE  [public_health_case_uid] = @dbo_Act_act_uid; 
