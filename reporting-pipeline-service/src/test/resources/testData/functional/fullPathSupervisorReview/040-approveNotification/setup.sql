498dd76e3bcb516be5016081c1e02eaaf5b666bb
Michael Peels <m.peels.dev@gmail.com>
2026-04-24 16:11:57 -0400

    Update notification checks

--- reporting-pipeline-service/src/test/resources/testData/functional/fullPathSupervisorReview/040-approveNotification/setup.sql
+++ reporting-pipeline-service/src/test/resources/testData/functional/fullPathSupervisorReview/040-approveNotification/setup.sql
@@ -9,18 +9,18 @@ DECLARE @dbo_Act_act_uid BIGINT = 20100408;
 -- dbo.Notification
 -- step: 4
 UPDATE [dbo].[notification]
-SET    [last_chg_time] = N'2026-04-22T20:52:44.717',
+SET    [last_chg_time] = N'2026-04-24T15:24:03.983',
        [last_chg_user_id] = @superuser_id,
        [record_status_cd] = N'APPROVED',
-       [record_status_time] = N'2026-04-22T20:52:44.717',
+       [record_status_time] = N'2026-04-24T15:24:03.983',
        [txt] = N'Notification Approved 04/21/2026',
        [version_ctrl_nbr] = Isnull([version_ctrl_nbr], 0) + 1
 WHERE  [notification_uid] = @dbo_Act_act_uid_2;
 
 -- dbo.PublicHealthCaseFact
--- step: 4
+-- step: 1
 UPDATE [dbo].[publichealthcasefact]
-SET    [lastnotificationdate] = N'2026-04-22T20:52:44.717',
+SET    [lastnotificationdate] = N'2026-04-24T15:24:03.983',
        [lastnotificationsubmittedby] = @superuser_id,
        [notifcurrentstate] = N'APPROVED',
        [notitxt] = N'Notification Approved 04/21/2026'
