c97e57d3ffd785bbeff439b19f88216903752d6f
Eric Buckley <eric.buckley@gmail.com>
2026-04-28 15:09:31 -0700

    removing semicolon

--- reporting-pipeline-service/src/test/resources/testData/unit/covidCaseDatamart/query.sql
+++ reporting-pipeline-service/src/test/resources/testData/unit/covidCaseDatamart/query.sql
@@ -2,4 +2,4 @@ EXEC rdb_modern..sp_covid_case_datamart_postprocessing 10009289;
 
 SELECT *
 FROM rdb_modern..covid_case_datamart
-WHERE public_health_case_uid = 10009289;
+WHERE public_health_case_uid = 10009289
