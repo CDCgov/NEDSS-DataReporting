6db2432fbb73bc2039a11d08599423322fc24cc0
Eric Buckley <eric.buckley@gmail.com>
2026-04-29 17:03:38 -0700

    more progress on constructing a good setup.sql to seed both legacy and modern covid_case_datamart stored procs

--- reporting-pipeline-service/src/test/resources/testData/unit/covidCaseDatamart/query.sql
+++ reporting-pipeline-service/src/test/resources/testData/unit/covidCaseDatamart/query.sql
@@ -1,2 +1,5 @@
-exec rdb_modern..sp_covid_case_datamart_postprocessing '10009289';
-SELECT * FROM dbo.COVID_CASE_DATAMART WHERE public_health_case_uid = 10009289;
+-- Execute the covid case datamart stored procedure
+EXEC rdb_modern..sp_covid_case_datamart_postprocessing 10009289;
+-- Return the processed covid case data for 10009289
+SELECT * FROM dbo.covid_case_datamart
+WHERE public_health_case_uid = 10009289;
