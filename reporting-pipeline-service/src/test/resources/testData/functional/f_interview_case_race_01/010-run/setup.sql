6e1a34d0a4e9a431ac4f7c43c3b1165f845afcf4
Patrick Hawley <patrick.hawley@skylight.digital>
2026-05-29 20:00:10 -0400

    try to get race condition to happen with a functional test

---
+++ reporting-pipeline-service/src/test/resources/testData/functional/f_interview_case_race_01/010-run/setup.sql
@@ -0,0 +1,14 @@
+-- One of 10 parallel functional tests for the F_INTERVIEW_CASE duplicate-insert race.
+-- Each copy seeds a different INTERVIEW_UID mapped to the same D_INTERVIEW_KEY
+-- (1000010000) and calls sp_f_interview_case_postprocessing. The functional test
+-- runner executes these concurrently. With the patch, all calls produce exactly
+-- one row. Without it, the race produces duplicates and the COUNT exceeds 1.
+
+USE RDB_MODERN;
+
+SET IDENTITY_INSERT dbo.nrt_interview_key ON;
+INSERT INTO dbo.nrt_interview_key (d_interview_key, interview_uid, created_dttm, updated_dttm)
+VALUES (1000010000, 1000010001, SYSDATETIME(), SYSDATETIME());
+SET IDENTITY_INSERT dbo.nrt_interview_key OFF;
+
+EXEC dbo.sp_f_interview_case_postprocessing @ix_uids = N'1000010001', @debug = 0;
