c6cc8467cce311c06c180d1a05438ed58e359b3e
Eric Nagel <eric@ericnagel.com>
2026-06-02 10:57:40 -0400

    Make sure the column isn't already varchar(100) NULL before altering

--- liquibase-service/src/main/resources/db/005-rdb_modern/tables/261-alter_std_hiv_case_management_widths-001.sql
+++ liquibase-service/src/main/resources/db/005-rdb_modern/tables/261-alter_std_hiv_case_management_widths-001.sql
@@ -1,34 +1,82 @@
-IF EXISTS (SELECT 1 FROM sys.columns WHERE object_id = OBJECT_ID(N'[dbo].[nrt_investigation_case_management]') AND name = 'fl_fup_actual_ref_type')
+IF EXISTS (
+    SELECT 1
+    FROM sys.columns
+    WHERE object_id = OBJECT_ID(N'[dbo].[nrt_investigation_case_management]')
+      AND name = 'fl_fup_actual_ref_type'
+      AND system_type_id = 167
+      AND collation_name IS NOT NULL
+      AND max_length <> 100
+)
 BEGIN
     ALTER TABLE [dbo].[nrt_investigation_case_management]
         ALTER COLUMN [fl_fup_actual_ref_type] varchar(100) NULL;
 END;
 
-IF EXISTS (SELECT 1 FROM sys.columns WHERE object_id = OBJECT_ID(N'[dbo].[nrt_investigation_case_management]') AND name = 'fl_fup_notification_plan_cd')
+IF EXISTS (
+    SELECT 1
+    FROM sys.columns
+    WHERE object_id = OBJECT_ID(N'[dbo].[nrt_investigation_case_management]')
+      AND name = 'fl_fup_notification_plan_cd'
+      AND system_type_id = 167
+      AND collation_name IS NOT NULL
+      AND max_length <> 100
+)
 BEGIN
     ALTER TABLE [dbo].[nrt_investigation_case_management]
         ALTER COLUMN [fl_fup_notification_plan_cd] varchar(100) NULL;
 END;
 
-IF EXISTS (SELECT 1 FROM sys.columns WHERE object_id = OBJECT_ID(N'[dbo].[D_CASE_MANAGEMENT]') AND name = 'FL_FUP_ACTUAL_REF_TYPE')
+IF EXISTS (
+    SELECT 1
+    FROM sys.columns
+    WHERE object_id = OBJECT_ID(N'[dbo].[D_CASE_MANAGEMENT]')
+      AND name = 'FL_FUP_ACTUAL_REF_TYPE'
+      AND system_type_id = 167
+      AND collation_name IS NOT NULL
+      AND max_length <> 100
+)
 BEGIN
     ALTER TABLE [dbo].[D_CASE_MANAGEMENT]
         ALTER COLUMN [FL_FUP_ACTUAL_REF_TYPE] varchar(100) NULL;
 END;
 
-IF EXISTS (SELECT 1 FROM sys.columns WHERE object_id = OBJECT_ID(N'[dbo].[D_CASE_MANAGEMENT]') AND name = 'FL_FUP_NOTIFICATION_PLAN_CD')
+IF EXISTS (
+    SELECT 1
+    FROM sys.columns
+    WHERE object_id = OBJECT_ID(N'[dbo].[D_CASE_MANAGEMENT]')
+      AND name = 'FL_FUP_NOTIFICATION_PLAN_CD'
+      AND system_type_id = 167
+      AND collation_name IS NOT NULL
+      AND max_length <> 100
+)
 BEGIN
     ALTER TABLE [dbo].[D_CASE_MANAGEMENT]
         ALTER COLUMN [FL_FUP_NOTIFICATION_PLAN_CD] varchar(100) NULL;
 END;
 
-IF EXISTS (SELECT 1 FROM sys.columns WHERE object_id = OBJECT_ID(N'[dbo].[STD_HIV_DATAMART]') AND name = 'FL_FUP_ACTUAL_REF_TYPE')
+IF EXISTS (
+    SELECT 1
+    FROM sys.columns
+    WHERE object_id = OBJECT_ID(N'[dbo].[STD_HIV_DATAMART]')
+      AND name = 'FL_FUP_ACTUAL_REF_TYPE'
+      AND system_type_id = 167
+      AND collation_name IS NOT NULL
+      AND max_length <> 100
+)
 BEGIN
     ALTER TABLE [dbo].[STD_HIV_DATAMART]
         ALTER COLUMN [FL_FUP_ACTUAL_REF_TYPE] varchar(100) NULL;
 END;
 
-IF EXISTS (SELECT 1 FROM sys.columns WHERE object_id = OBJECT_ID(N'[dbo].[STD_HIV_DATAMART]') AND name = 'FL_FUP_NOTIFICATION_PLAN')
+IF EXISTS (
+    SELECT 1
+    FROM sys.columns
+    WHERE object_id = OBJECT_ID(N'[dbo].[STD_HIV_DATAMART]')
+      AND name = 'FL_FUP_NOTIFICATION_PLAN'
+      AND system_type_id = 167
+      AND collation_name IS NOT NULL
+      AND max_length <> 100
+)
 BEGIN
     ALTER TABLE [dbo].[STD_HIV_DATAMART]
         ALTER COLUMN [FL_FUP_NOTIFICATION_PLAN] varchar(100) NULL;
