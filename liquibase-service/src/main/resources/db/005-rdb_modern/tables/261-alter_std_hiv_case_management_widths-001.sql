b53a239c0e0e0b5e3fd5ac4c019f9a80351c293f
Eric Nagel <eric@ericnagel.com>
2026-06-04 11:40:43 -0400

    Needed to increase this column, too

--- liquibase-service/src/main/resources/db/005-rdb_modern/tables/261-alter_std_hiv_case_management_widths-001.sql
+++ liquibase-service/src/main/resources/db/005-rdb_modern/tables/261-alter_std_hiv_case_management_widths-001.sql
@@ -81,3 +81,17 @@ BEGIN
     ALTER TABLE [dbo].[STD_HIV_DATAMART]
         ALTER COLUMN [FL_FUP_NOTIFICATION_PLAN] varchar(100) NULL;
 END;
+
+IF EXISTS (
+    SELECT 1
+    FROM sys.columns
+    WHERE object_id = OBJECT_ID(N'[dbo].[DM_INV_HIV]')
+      AND name = 'FL_FUP_ACTUAL_REF_TY'
+      AND system_type_id = 167
+      AND collation_name IS NOT NULL
+      AND max_length <> 100
+)
+BEGIN
+    ALTER TABLE [dbo].[DM_INV_HIV]
+        ALTER COLUMN [FL_FUP_ACTUAL_REF_TY] varchar(25) NULL;
+END;
