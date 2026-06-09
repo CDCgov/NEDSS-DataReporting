55e61eb8ad381a4a6ae4e72d5ada2e12cb7cbb8c
Michael Peels <m.peels.dev@gmail.com>
2026-06-09 11:49:17 -0400

    Automatic liquibase run

--- reporting-pipeline-service/src/test/java/gov/cdc/nbs/report/pipeline/integration/unit/UnitTest.java
+++ reporting-pipeline-service/src/test/java/gov/cdc/nbs/report/pipeline/integration/unit/UnitTest.java
@@ -3,18 +3,12 @@ package gov.cdc.nbs.report.pipeline.integration.unit;
 import gov.cdc.nbs.report.pipeline.integration.support.config.DataSourceConfig;
 import java.io.File;
 import java.time.Duration;
-import javax.sql.DataSource;
-import liquibase.exception.LiquibaseException;
-import liquibase.integration.spring.SpringLiquibase;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -24,23 +18,17 @@ import org.testcontainers.containers.ComposeContainer;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.containers.wait.strategy.Wait;
 
-@DataJpaTest()
+@DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 @TestInstance(Lifecycle.PER_CLASS)
 @ActiveProfiles("test")
 @Tag("Unit")
 @Import(DataSourceConfig.class)
 public abstract class UnitTest {
-  
-  @Autowired
-  @Qualifier("adminDataSource")
-  private DataSource liquibaseDatasource;
 
   private static final Logger log = LoggerFactory.getLogger(UnitTest.class);
   private static final Slf4jLogConsumer consumer = new Slf4jLogConsumer(log);
   private static final File base = new File("../docker-compose.yaml");
-
-  private static boolean started = false;
 
   @SuppressWarnings("resource")
   private static ComposeContainer environment =
@@ -51,36 +39,14 @@ public abstract class UnitTest {
           // Set the maximum startup timeout all the waits set are bounded to
           .withStartupTimeout(Duration.ofMinutes(10));
 
-
-  private void runLiquibase() {
-        SpringLiquibase liquibase = new SpringLiquibase();
-        liquibase.setDataSource(liquibaseDatasource);
-        liquibase.setChangeLog("classpath:db/changelog/db.changelog-sample.yaml");
-        try {
-          liquibase.afterPropertiesSet();
-        } catch (LiquibaseException e) {
-          throw new RuntimeException(e.getMessage());
-        } 
-  }
-
-  @BeforeAll
-  void setUp()  {
-    synchronized (UnitTest.class) {
-      if (!started) {
-        environment.start();
-        started = true;
-        runLiquibase();
-      }
-    }
+  static {
+    // Must be statically initialized to allow liquibase configuration
+    // to run without manual intervention
+    environment.start();
   }
 
   @AfterAll
-  void tearDown() {
-    synchronized (UnitTest.class) {
-      if (started) {
-        environment.stop();
-        started = false;
-      }
-    }
+  static void tearDown() {
+    environment.stop();
   }
 }
