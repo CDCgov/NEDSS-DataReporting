19d9dc0047359863ecb62499de878b3593287a2b
Eric Buckley <eric.buckley@gmail.com>
2026-07-01 10:13:12 -0700

    (db): lower compatibility level to 130
    The database migration script previously set the compatibility level to 150
    (SQL Server 2019), which is too high for some STLTs. It is now adjusted to
    130 (SQL Server 2016), when string split was added. Formatting of the SQL
    query has also been improved for better readability.

--- reporting-pipeline-service/src/main/resources/db/changelog/migrations/v17.3/rdb/000-rdbmodern-db-general-001.sql
+++ reporting-pipeline-service/src/main/resources/db/changelog/migrations/v17.3/rdb/000-rdbmodern-db-general-001.sql
@@ -1,8 +1,14 @@
 -- Query update to run against RDB with RDB_modern compatibility
 -- Upgrade compatibility level to allow inbuilt functions such as StringSplit
 DECLARE @CURRENT_DB NVARCHAR(128) = db_name()
-IF (SELECT COMPATIBILITY_LEVEL FROM sys.databases WHERE name =  @CURRENT_DB)<150
+IF
+    (
+        SELECT COMPATIBILITY_LEVEL FROM SYS.DATABASES
+        WHERE NAME = @CURRENT_DB
+    ) < 130
     BEGIN
-        EXEC('ALTER DATABASE '+@CURRENT_DB+' SET COMPATIBILITY_LEVEL = 150');
-        PRINT 'Updated ' + @CURRENT_DB + ' to compatibility level 150.'
+        EXEC (
+            'ALTER DATABASE ' + @CURRENT_DB + ' SET COMPATIBILITY_LEVEL = 130'
+        );
+        PRINT 'Updated ' + @CURRENT_DB + ' to compatibility level 130.'
     END
