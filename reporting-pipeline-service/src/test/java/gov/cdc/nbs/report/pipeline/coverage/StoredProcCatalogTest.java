a1590591d1b698407a2034546af03bd12ba01bcc
Michael Peels <m.peels.dev@gmail.com>
2026-07-29 12:23:05 -0400

    Fix storedProcCatalogTest. This might need changing

--- reporting-pipeline-service/src/test/java/gov/cdc/nbs/report/pipeline/coverage/StoredProcCatalogTest.java
+++ reporting-pipeline-service/src/test/java/gov/cdc/nbs/report/pipeline/coverage/StoredProcCatalogTest.java
@@ -12,12 +12,12 @@ class StoredProcCatalogTest {
 
   @Test
   void finds_every_routine_file() {
-    assertEquals(130, catalog.size());
+    assertEquals(132, catalog.size());
   }
 
   @Test
   void counts_procs_that_log_to_job_flow_log() {
-    assertEquals(128, StoredProcCatalog.loggingProcCount(catalog));
+    assertEquals(130, StoredProcCatalog.loggingProcCount(catalog));
   }
 
   @Test
