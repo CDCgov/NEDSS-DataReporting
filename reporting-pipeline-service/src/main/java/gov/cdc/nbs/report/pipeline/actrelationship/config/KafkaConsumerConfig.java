499c713520073b85049c6bfbc70576351f548351
Michael Peels <m.peels.dev@gmail.com>
2026-08-11 10:53:53 -0400

    name KafkaConsumerConfig

--- reporting-pipeline-service/src/main/java/gov/cdc/nbs/report/pipeline/actrelationship/config/KafkaConsumerConfig.java
+++ reporting-pipeline-service/src/main/java/gov/cdc/nbs/report/pipeline/actrelationship/config/KafkaConsumerConfig.java
@@ -11,7 +11,7 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
-@Configuration
+@Configuration("actRelationshipKafkaConsumerConfig")
 public class KafkaConsumerConfig {
 
   @Value("${spring.kafka.group-id}")
