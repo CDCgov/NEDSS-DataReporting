0ebdd969bd1bb65f02daf2e8515a51da7e538ab0
Michael Peels <m.peels.dev@gmail.com>
2026-08-11 10:48:45 -0400

    Remove unused import

--- reporting-pipeline-service/src/test/java/gov/cdc/nbs/report/pipeline/investigation/service/InvestigationServiceTest.java
+++ reporting-pipeline-service/src/test/java/gov/cdc/nbs/report/pipeline/investigation/service/InvestigationServiceTest.java
@@ -32,7 +32,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.*;
 import org.springframework.kafka.core.KafkaTemplate;
