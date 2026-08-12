a82317ee5946763d31b4d5e5855c138da64e0908
Eli <elijah.reyes@skylight.digital>
2026-08-12 11:54:54 -0400

    moving cache locks to new repo with 7.13 base

--- reporting-pipeline-service/src/main/java/gov/cdc/nbs/report/pipeline/postprocessing/service/ProcessDatamartData.java
+++ reporting-pipeline-service/src/main/java/gov/cdc/nbs/report/pipeline/postprocessing/service/ProcessDatamartData.java
@@ -60,7 +60,7 @@ public class ProcessDatamartData {
 
   private final CustomJsonGeneratorImpl jsonGenerator = new CustomJsonGeneratorImpl();
   private final ModelMapper modelMapper = new ModelMapper();
-  private final Object cacheLock = new Object();
+  private final Object retryCacheLock = new Object();
 
   // set of caches for retrying failed datamarts/IDs and tracking retry attempts
   final Map<Long, Map<String, Map<String, Queue<Long>>>> retryCache = new ConcurrentHashMap<>();
@@ -660,7 +660,7 @@ public class ProcessDatamartData {
       // creates a deep copy of cache into snapshot
       final Map<String, Map<String, List<Long>>> retryCacheSnapshot;
 
-      synchronized (cacheLock) {
+      synchronized (retryCacheLock) {
         retryCacheSnapshot =
             retryEntry.getValue().entrySet().stream()
                 .collect(
@@ -923,6 +923,7 @@ public class ProcessDatamartData {
     }
 
     private void updateRetryCache(String dmKey, Map<String, List<Long>> idMap) {
+
       Map<String, Queue<Long>> dmMap =
           retryCache
               .computeIfAbsent(batchId, k -> new ConcurrentHashMap<>())
