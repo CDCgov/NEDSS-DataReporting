4508bcaf317207307dfc561af26f0e066b5fe0fc
Eli <elijah.reyes@skylight.digital>
2026-08-12 14:39:22 -0400

    removed unnecessary locks

--- reporting-pipeline-service/src/main/java/gov/cdc/nbs/report/pipeline/postprocessing/service/PostProcessingService.java
+++ reporting-pipeline-service/src/main/java/gov/cdc/nbs/report/pipeline/postprocessing/service/PostProcessingService.java
@@ -359,11 +359,8 @@ public class PostProcessingService {
           Arrays.stream(tblNode.asText().split(","))
               .map(String::trim)
               .forEach(
-                  tbl -> {
-                    synchronized (cacheLock) {
-                      pbCache.computeIfAbsent(tbl, k -> new ConcurrentLinkedQueue<>()).add(uid);
-                    }
-                  });
+                  tbl ->
+                      pbCache.computeIfAbsent(tbl, k -> new ConcurrentLinkedQueue<>()).add(uid));
         }
       } else if (topic.endsWith(NOTIFICATION.getEntityName())) {
         String actTypeCd = payloadNode.path("act_type_cd").asText();
@@ -379,9 +376,7 @@ public class PostProcessingService {
 
         if (MORB_REPORT.equals(ctrlCd)) {
           if ("Order".equals(domainCd)) {
-            synchronized (cacheLock) {
               obsCache.computeIfAbsent(MORB_REPORT, k -> new ConcurrentLinkedQueue<>()).add(uid);
-            }
           }
         } else if (assertMatches(ctrlCd, LAB_REPORT, LAB_REPORT_MORB, null)
             && assertMatches(
@@ -393,9 +388,7 @@ public class PostProcessingService {
                 "I_Order",
                 "I_Result",
                 "Order_rslt")) {
-          synchronized (cacheLock) {
             obsCache.computeIfAbsent(LAB_REPORT, k -> new ConcurrentLinkedQueue<>()).add(uid);
-          }
         }
 
         /*
@@ -431,14 +424,10 @@ public class PostProcessingService {
 
   private void extractSummaryCase(Long uid, String caseType) {
     if (ACT_TYPE_SUM.equals(caseType) || "S".equals(caseType)) {
-      synchronized (cacheLock) {
         sumCache.computeIfAbsent(CASE_TYPE_SUM, k -> new ConcurrentLinkedQueue<>()).add(uid);
-      }
     }
     if (ACT_TYPE_SUM.equals(caseType) || "A".equals(caseType)) {
-      synchronized (cacheLock) {
         sumCache.computeIfAbsent(CASE_TYPE_AGG, k -> new ConcurrentLinkedQueue<>()).add(uid);
-      }
     }
   }
 
